7513d8397303d8edc3941b1efd69089f509a7115
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-02 12:40:40 +0100

    Removed unnecessary includes from TestRunner

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -1,29 +1,23 @@
-#include <iostream>
-#include <string>
-#include <list>
-#include <cppunit/TestCase.h>
-#include <cppunit/TestFixture.h>
-#include <cppunit/ui/text/TextTestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
-#include <cppunit/TestRunner.h>
 #include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/XmlOutputter.h>
-#include <netinet/in.h>
+
+using namespace CppUnit;
+
 
 #include "./TestBasicMath.cpp"
 
-using namespace CppUnit;
-using namespace std;
-
 CPPUNIT_TEST_SUITE_REGISTRATION( TestBasicMath );
+
 
 int main(int argc, char* argv[])
 {
-    // informs test-listener about testresults
+    // informs test-listener about test results
     CPPUNIT_NS::TestResult testresult;
 
     // register listener for collecting the test-results
