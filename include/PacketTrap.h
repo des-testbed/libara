dd63d953d5c1f36c5ddf37f310b108215d301209
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-10 09:08:15 +0100

    Work in progress on the first PacketTrap implementation

---
+++ include/PacketTrap.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ Copyright 2012, The DES-SERT Team, Freie Universität Berlin (FUB).
+ All rights reserved.
+
+ These sources were originally developed by Friedrich Große
+ at Freie Universität Berlin (http://www.fu-berlin.de/),
+ Computer Systems and Telematics / Distributed, Embedded Systems (DES) group
+ (http://cst.mi.fu-berlin.de/, http://www.des-testbed.net/)
+ ------------------------------------------------------------------------------
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program. If not, see http://www.gnu.org/licenses/ .
+ ------------------------------------------------------------------------------
+ For further information and questions please use the web site
+ http://www.des-testbed.net/
+ *******************************************************************************/
+
+#ifndef PACKETTRAP_H_
+#define PACKETTRAP_H_
+
+#include <ext/hash_map>
+#include "Packet.h"
+#include "Address.h"
+
+namespace ARA {
+
+class PacketTrap {
+public:
+    void trapPacket(Packet* packet);
+
+private:
+    //FIXME __gnu_cxx::hash_map<Address, Packet, AddressHash> trappedPackets;
+};
+
+} /* namespace ARA */
+#endif /* PACKETTRAP_H_ */
---
+++ tests/PacketTrapTest.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ Copyright 2012, The DES-SERT Team, Freie Universität Berlin (FUB).
+ All rights reserved.
+
+ These sources were originally developed by Friedrich Große
+ at Freie Universität Berlin (http://www.fu-berlin.de/),
+ Computer Systems and Telematics / Distributed, Embedded Systems (DES) group
+ (http://cst.mi.fu-berlin.de/, http://www.des-testbed.net/)
+ ------------------------------------------------------------------------------
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program. If not, see http://www.gnu.org/licenses/ .
+ ------------------------------------------------------------------------------
+ For further information and questions please use the web site
+ http://www.des-testbed.net/
+ *******************************************************************************/
+
+#include <UnitTest++.h>
+#include "PacketTrap.h"
+#include "Packet.h"
+#include "mocks/PacketMock.h"
+
+using namespace ARA;
+
+SUITE(PacketTrapTest) {
+
+    TEST(testTrapPacket) {
+        PacketTrap packetTrap = PacketTrap();
+        Packet* packet = new PacketMock();
+        //TODO packetTrap.trapPacket(packet);
+
+        //TODO CHECK(packetTrap.trappedPackets.contains(packet));
+
+        delete packet;
+    }
+
+  }
