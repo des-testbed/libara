15e730c1e4ef8f657224d7ae0a4292c9e132eb4e
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-12 22:53:51 +0100

    LinkedListTest does now longer use dynamically allocated memory (new)

--- tests/util/LinkedListTest.cpp
+++ tests/util/LinkedListTest.cpp
@@ -40,52 +40,41 @@ SUITE(LinkedListTest) {
 
     TEST(testAddAndGet) {
         LinkedList<string> list = LinkedList<string>();
+        string foo = string("Hello Foo World");
+        string bar = string("Hello Bar World");
+        string fooBar = string("Hello FooBar World");
 
-        string* foo = new string("Hello Foo World");
-        list.add(foo);
+        list.add(&foo);
+        list.add(&bar);
+        list.add(&fooBar);
 
-        string* bar = new string("Hello Bar World");
-        list.add(bar);
-
-        string* fooBar = new string("Hello FooBar World");
-        list.add(fooBar);
-
-        CHECK_EQUAL(foo, list.get(0));
-        CHECK_EQUAL(fooBar, list.get(2));
-        CHECK_EQUAL(bar, list.get(1));
-
-        delete foo;
-        delete bar;
-        delete fooBar;
+        CHECK_EQUAL(foo, *list.get(0));
+        CHECK_EQUAL(fooBar, *list.get(2));
+        CHECK_EQUAL(bar, *list.get(1));
     }
 
     TEST(testGetThrowsExceptionIfIndexIsOutOfBounds) {
         LinkedList<string> list = LinkedList<string>();
         CHECK_THROW(list.get(0), ARA::Exception);
 
-        string* foo = new string("Hello Foo World");
-        list.add(foo);
+        string foo = string("Hello Foo World");
+        list.add(&foo);
         list.get(0);    // This should now work
 
-        string* bar = new string("Hello Bar World");
-        list.add(bar);
+        string bar = string("Hello Bar World");
+        list.add(&bar);
         list.get(1);    // This should now work
 
         CHECK_THROW(list.get(2), Exception);
-
-        delete foo;
-        delete bar;
     }
 
     TEST(testIsEmpty) {
         LinkedList<string> list = LinkedList<string>();
-        string* test = new string("Hello World");
+        string test = string("Hello World");
 
         CHECK(list.isEmpty() == true);
-        list.add(test);
+        list.add(&test);
         CHECK(list.isEmpty() == false);
-
-        delete test;
     }
 
     TEST(DeletingFromEmtyListthrowsException) {
@@ -95,24 +84,21 @@ SUITE(LinkedListTest) {
 
     TEST(testRemove) {
         LinkedList<string> list = LinkedList<string>();
-        string* foo = new string("Foo");
-        string* bar = new string("Bar");
-        string* hello = new string("Hello World");
+        string foo = string("Foo");
+        string bar = string("Bar");
+        string hello = string("Hello World");
 
-        list.add(foo);
-        list.add(bar);
-        list.add(hello);
+        list.add(&foo);
+        list.add(&bar);
+        list.add(&hello);
 
         CHECK(list.isEmpty() == false);
 
-        CHECK_EQUAL(foo, list.remove());
-        CHECK_EQUAL(bar, list.remove());
-        CHECK_EQUAL(hello, list.remove());
+        CHECK_EQUAL(foo, *list.remove());
+        CHECK_EQUAL(bar, *list.remove());
+        CHECK_EQUAL(hello, *list.remove());
 
         CHECK(list.isEmpty() == true);
-
-        delete foo;
-        delete bar;
-        delete hello;
     }
+
 }
