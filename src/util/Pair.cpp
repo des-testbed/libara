2a0eb4c2f9ca24860d97433f14981ef02921e07b
Friedrich Große <friedrich.grosse@gmail.com>
2012-12-13 15:24:34 +0100

    Added another utility container class: Pair which represents a two element set

---
+++ src/util/Pair.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+ Copyright 2012, The DES-SERT Team, Freie Universität Berlin (FUB).
+ All rights reserved.
+
+ These sources were originally developed by Friedrich Große
+ at Freie Universität Berlin (http://www.fu-berlin.de/),
+ Computer Systems and Telematics / Distributed, Embedded Systems (DES) group
+ (http://cst.mi.fu-berlin.de/, http://www.des-testbed.net/)
+ ------------------------------------------------------------------------------
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program. If not, see http://www.gnu.org/licenses/ .
+ ------------------------------------------------------------------------------
+ For further information and questions please use the web site
+ http://www.des-testbed.net/
+ *******************************************************************************/
+
+#define PAIR_H_CPP_
+#include "Pair.h"
+
+namespace ARA {
+
+template<class Left, class Right>
+Pair<Left, Right>::Pair(Left* left, Right* right) {
+    this->left = left;
+    this->right = right;
+}
+
+template<class Left, class Right>
+Left* Pair<Left, Right>::getLeft() {
+    return left;
+}
+
+template<class Left, class Right>
+Right* Pair<Left, Right>::getRight() {
+    return right;
+}
+
+} /* namespace ARA */
