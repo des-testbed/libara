df2b7009e52fcbb001882200ec3dd455f073c1a4
Friedrich Große <friedrich.grosse@gmail.com>
2013-01-09 20:50:42 +0100

    Created the most basic OMNeTARAClient which is a subclass of cSimpleModule and AbstractARAClient.

---
+++ include/OMNeTARAClient.h
@@ -0,0 +1,41 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef OMNETARACLIENT_H_
+#define OMNETARACLIENT_H_
+
+#include <csimplemodule.h>
+#include "AbstractARAClient.h"
+#include "NetworkInterface.h"
+#include "NextHop.h"
+#include "Packet.h"
+
+namespace ARA {
+
+class OMNeTARAClient: public cSimpleModule, AbstractARAClient {
+
+protected:
+    //~~~ INHERITED FROM cSimpleModule ~~~~~~~
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+
+    //~~~ INHERITED FROM AbstractARAClient ~~~
+    NextHop* getNextHop(const Packet* packet);
+    void updateRoutingTable(const Packet* packet, NetworkInterface* interface);
+    void deliverToSystem(const Packet* packet);
+};
+
+} /* namespace ARA */
+#endif /* OMNETARACLIENT_H_ */
---
+++ src/omnetpp/OMNeTARAClient.cpp
@@ -0,0 +1,64 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "OMNeTARAClient.h"
+#include <omnetpp.h>
+
+using namespace ARA;
+
+// The module class needs to be registered with OMNeT++
+Define_Module(OMNeTARAClient);
+
+void OMNeTARAClient::initialize() {
+    if (strcmp("source", getName()) == 0) {
+        cMessage* msg = new cMessage("Foo");
+        send(msg, "g$o", 0);
+    }
+}
+
+void OMNeTARAClient::handleMessage(cMessage *msg) {
+    send(msg, "g$o", 0);
+}
+
+NextHop* OMNeTARAClient::getNextHop(const Packet* packet) {
+    if(routingTable.isDeliverable(packet)) {
+        LinkedList<RoutingTableEntry>* possibleHops = routingTable.getPossibleNextHops(packet);
+        // search for the best value
+        // TODO this can be replaced as soon as Michael is ready with the corresponding class
+        RoutingTableEntry* bestHop = NULL;
+
+        unsigned int nrOfPossibleRoutes = possibleHops->size();
+        for (unsigned int i = 0; i < nrOfPossibleRoutes; ++i) {
+            RoutingTableEntry* currentHop = possibleHops->get(i);
+            if(bestHop == NULL || currentHop->getPheromoneValue() > bestHop->getPheromoneValue()) {
+                bestHop = currentHop;
+            }
+        }
+
+        return bestHop->getNextHop();
+    }
+    else {
+        // TODO maybe it would be better to return a NULL Object (Pattern) instead of returning a NULL pointer
+        return NULL;
+    }
+}
+
+void OMNeTARAClient::updateRoutingTable(const Packet* packet, NetworkInterface* interface) {
+    // TODO implement OMNeTARAClient::updateRoutingTable
+}
+
+void OMNeTARAClient::deliverToSystem(const Packet* packet) {
+    EV << getName() << " delivered a packet to the system\n";
+}
