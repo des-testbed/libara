995074fcaf08b35f9a79fdb6d324c1367f6e0949
Michael Frey <frey@inf.fu-berlin.de>
2013-01-31 17:28:51 +0100

    added stub for class CubicEvaporationPolicy

---
+++ include/CubicEvaporationPolicy.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+ Copyright 2012, The DES-SERT Team, Freie Universität Berlin (FUB).
+ All rights reserved.
+
+ These sources were originally developed by Friedrich Große, Michael Frey
+ at Freie Universität Berlin (http://www.fu-berlin.de/),
+ Computer Systems and Telematics / Distributed, Embedded Systems (DES) group
+ (http://cst.mi.fu-berlin.de/, http://www.des-testbed.net/)
+ ------------------------------------------------------------------------------
+ This program is free software: you can redistribute it and/or modify it under
+ the terms of the GNU General Public License as published by the Free Software
+ Foundation, either version 3 of the License, or (at your option) any later
+ version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with
+ this program. If not, see http://www.gnu.org/licenses/ .
+ ------------------------------------------------------------------------------
+ For further information and questions please use the web site
+ http://www.des-testbed.net/
+ *******************************************************************************/
+
+#ifndef LINEAR_EVAPORATION_POLICY_H_
+#define LINEAR_EVAPORATION_POLICY_H_
+
+#include <cmath>
+#include <cstdlib>
+#include <stdint.h>
+
+#include "EvaporationPolicy.h"
+#include "NoRemainingPheromoneException.h"
+
+namespace ARA { 
+   /**
+    * This class provides the linear evaporation function of the ant routing algorithm (ARA).
+    */
+    class CubicEvaporationPolicy : public EvaporationPolicy {
+        public:
+            CubicEvaporationPolicy();
+            /// the method reduces the pheromone value of a routing table entry
+            void evaporate(float*, uint8_t);
+        private:
+            /// 
+            float plateau;
+            /// 
+            float reduction;
+            ///
+            float slow;
+            /// the threshold which denotes at what point the pheromone level is set to 0
+            float threshold;
+    };
+} /* namespace ARA */
+
+#endif 
--- src/core/LinearEvaporationPolicy.cpp
+++ src/core/LinearEvaporationPolicy.cpp
@@ -9,8 +9,8 @@ LinearEvaporationPolicy::LinearEvaporationPolicy():q(0.1),threshold(0.2){};
  * 
  */
 void LinearEvaporationPolicy::evaporate(float *pheromone, uint8_t factor){
-   /// do the computation
-   *pheromone = pow(((1 - this->q) * (*pheromone)), factor);
+   /// do the computation 
+   *pheromone = pow(((1 - this->q) * (*pheromone)), factor); 
 
    /// check if the result is below a threshold
    if(*pheromone < this->threshold){
