abb359d135cc8bf3362452a76a404d8ab893299f
Friedrich Große <friedrich.grosse@gmail.com>
2013-02-16 07:56:18 +0100

    Created a NetworkConfigurator that just assigns IP addresses to all nodes

---
+++ include/omnetpp/ARANetworkConfigurator.h
@@ -0,0 +1,57 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __ARA_SIMULATION_NETWORKCONFIGURATOR_H_
+#define __ARA_SIMULATION_NETWORKCONFIGURATOR_H_
+
+#include "IPAddress.h"
+#include "IInterfaceTable.h"
+
+#include <omnetpp.h>
+
+namespace ARA {
+namespace omnetpp {
+
+/**
+ * The NetworkConfigurator assigns IPv4 addresses to all nodes in the network.
+ *
+ * Unlike the INET/MANET NetworkConfigurator classes its does not search for
+ * any routes or update any routing tables.
+ */
+class ARANetworkConfigurator : public cSimpleModule {
+protected:
+    struct NodeInfo {
+        NodeInfo() {hasInterfaceTable=false; interfaceTable=NULL;}
+        bool hasInterfaceTable;
+        IInterfaceTable* interfaceTable;
+        IPAddress address;
+    };
+    typedef std::vector<NodeInfo> NodeInfoVector;
+
+    virtual int numInitStages() const  {return 3;}
+    virtual void initialize(int stage);
+    virtual void handleMessage(cMessage *msg);
+
+    void extractTopology(cTopology& topo);
+    void assignAddresses(cTopology& topology);
+
+private:
+    NodeInfoVector nodeInfo;
+};
+
+} /* namespace omnetpp */
+} /* namespace ARA */
+
+#endif
