6a206f0732a9d8823a33b527da87aca7c578c772
Friedrich Große <friedrich.grosse@gmail.com>
2013-02-27 21:59:19 +0100

    Created the abstract TimeFactory class

---
+++ tests/testAPI/mocks/TimeFactoryMock.h
@@ -0,0 +1,13 @@
+#ifndef TIMEFACTORY_MOCK_H_
+#define TIMEFACTORY_MOCK_H_
+
+#include "TimeFactory.h"
+
+namespace ARA {
+    class TimeFactoryMock : public TimeFactory {
+        public:
+            Time* makeTime();
+    };
+}
+
+#endif
