2649e380aad82791b5ddd583b7d52d7e554f8178
Michael Frey <frey@inf.fu-berlin.de>
2013-03-15 16:44:27 +0100

    provided implementation for class EnergyAwareRoutingTable

--- include/EnergyAwareRoutingTable.h
+++ include/EnergyAwareRoutingTable.h
@@ -8,51 +8,20 @@
 #include "Packet.h"
 #include "Address.h"
 #include "NetworkInterface.h"
-#include "EvaporationPolicy.h"
+#include "RoutingTable.h"
 #include "EnergyAwareRoutingTableEntry.h"
-#include "LinearEvaporationPolicy.h"
-#include "TimeFactory.h"
 
 #include <deque>
 #include <unordered_map>
 
 namespace ARA {
+    class EnergyAwareRoutingTable : public RoutingTable {
+        public:
+           float getEnergyValue(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface);
+           void update(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface, float pheromoneValue, float energyValue);
 
-class EnergyAwareRoutingTable {
-
-public:
-    EnergyAwareRoutingTable(TimeFactory* timeFactory);
-    ~EnergyAwareRoutingTable();
-
-    float getEnergyValue(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface);
-    void update(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface, float pheromoneValue, float energyValue);
-    void removeEntry(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface);
-    std::deque<EnergyAwareRoutingTableEntry*>* getPossibleNextHops(std::shared_ptr<Address> destination);
-    std::deque<EnergyAwareRoutingTableEntry*>* getPossibleNextHops(const Packet* packet);
-    bool isDeliverable(std::shared_ptr<Address> destination);
-    bool isDeliverable(const Packet* packet);
-
-    bool exists(std::shared_ptr<Address> destination, std::shared_ptr<Address> nextHop, NetworkInterface* interface);
-
-    void setEvaporationPolicy(EvaporationPolicy *policy);
-    EvaporationPolicy *getEvaporationPolicy() const;
-    std::unordered_map<std::shared_ptr<Address>, std::deque<EnergyAwareRoutingTableEntry*>*, AddressHash, AddressPredicate> getEnergyAwareRoutingTable() const;
-    void setEnergyAwareRoutingTable(std::unordered_map<std::shared_ptr<Address>, std::deque<EnergyAwareRoutingTableEntry*>*, AddressHash, AddressPredicate> table);
-
-protected:
-    bool hasTableBeenAccessedEarlier();
-    void triggerEvaporation();
-
-    TimeFactory* timeFactory;
-    Time *lastAccessTime;
-
-private:
-    std::unordered_map<std::shared_ptr<Address>, std::deque<EnergyAwareRoutingTableEntry*>*, AddressHash, AddressPredicate> table;
-    /**
-     * The memory management of the evaporationPolicy member is handled in class
-     * OMNeTEARAClient. Thus, there is no delete call to the evaporationPolicy member.
-     */
-    EvaporationPolicy *evaporationPolicy;
+        protected:
+           void updateExistingEntry(RoutingTableEntry *oldEntry, RoutingTableEntry *newEntry);
 };
 
 } /* namespace ARA */
