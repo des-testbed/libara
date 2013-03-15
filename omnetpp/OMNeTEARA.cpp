02b19ac7086df67a043277a98cb5532f9f7c08f9
Michael Frey <frey@inf.fu-berlin.de>
2013-03-15 18:49:06 +0100

    added stub for class OMNeTEARA

---
+++ omnetpp/OMNeTEARA.cpp
@@ -0,0 +1,16 @@
+/*
+ * $FU-Copyright$
+ */
+
+#include "omnetpp/OMNeTEARA.h"
+
+/*
+namespace ARA {
+    namespace omnetpp {
+        typedef std::shared_ptr<Address> AddressPtr;
+
+        Define_Module(OMNeTEARA);
+
+    }
+}
+*/
