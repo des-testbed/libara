3a07ceed00914aee8b9ce2a6f99176096ab92ad8
Friedrich Große <friedrich.grosse@gmail.com>
2013-03-22 11:30:27 +0100

    Refactored the ARAClientMock: The basic mocking stuff is now in its own class
    This will ease the development of the EARAClient and Mock

---
+++ tests/testAPI/mocks/AbstractClientMockBase.h
@@ -0,0 +1,61 @@
+/*
+ * $FU-Copyright$
+ */
+
+#ifndef ABSTRACT_ARACLIENT_MOCK_BASE_H_
+#define ABSTRACT_ARACLIENT_MOCK_BASE_H_
+
+#include "AbstractARAClient.h"
+#include "NetworkInterfaceMock.h"
+#include "Configuration.h"
+#include "RoutingTable.h"
+
+#include <string>
+#include <deque>
+
+namespace ARA {
+
+/**
+ * The abstract AbstractClientMockBase supplies several mocking methods that can be
+ * used by its implementing mocks (ARACLientMock and EARAClientMock)
+ */
+class AbstractClientMockBase {
+public:
+    struct PacketInfo {
+        const Packet* packet;
+        std::shared_ptr<Address> nextHop;
+        const NetworkInterface* interface;
+    };
+
+    AbstractClientMockBase() {};
+    virtual ~AbstractClientMockBase();
+
+    void storeReceivedPacket(Packet* packet, NetworkInterface* interface);
+    void storeRouteFailurePacket(Packet* packet, std::shared_ptr<Address> nextHop, NetworkInterface* interface);
+    void storeDeliveredPacket(const Packet* packet);
+    void storeUndeliverablePacket(const Packet* packet);
+
+    // Mocking methods
+
+    std::deque<const Packet*>* getDeliveredPackets();
+    virtual NetworkInterfaceMock* createNewNetworkInterfaceMock(const std::string localAddressName = "DEFAULT");
+
+    int getNumberOfReceivedPackets();
+    std::deque<Pair<const Packet*, const NetworkInterface*>*> getReceivedPackets();
+
+    int getNumberOfRouteFailures();
+    std::deque<PacketInfo> getRouteFailurePackets();
+
+    int getNumberOfUndeliverablePackets();
+    std::deque<const Packet*> getUndeliverablePackets();
+
+protected:
+    std::deque<NetworkInterfaceMock*> interfaceMocks;
+    std::deque<const Packet*> deliveredPackets;
+    std::deque<Pair<const Packet*, const NetworkInterface*>*> receivedPackets;
+    std::deque<PacketInfo> routeFailurePackets;
+    std::deque<const Packet*> undeliverablePackets;
+};
+
+} /* namespace ARA */
+#endif /* ABSTRACT_ARACLIENT_MOCK_BASE_H_ */
