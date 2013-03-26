baad0c27e2c44c936edeadefaed0655ae3bef2a1
Michael Frey <frey@inf.fu-berlin.de>
2013-03-26 11:15:27 +0100

    added stub for linear evaporation policy

---
+++ src/core/evaporation/LinearEvaporationPolicy.cpp
@@ -0,0 +1,31 @@
+/*
+ * $FU-Copyright$
+ */
+
+#include "LinearEvaporationPolicy.h"
+
+#include <cmath>
+#include <iostream>
+
+using namespace ARA;
+
+LinearEvaporationPolicy::LinearEvaporationPolicy(float evaporationFactor, float threshold, unsigned int timeIntervalInMilliSeconds) : EvaporationPolicy(timeIntervalInMilliSeconds) {
+    this->evaporationFactor = evaporationFactor;
+    this->threshold = threshold;
+}
+
+//FIXME
+float LinearEvaporationPolicy::evaporate(float oldPheromoneValue, int millisecondsSinceLastEvaporation){
+    if(millisecondsSinceLastEvaporation == 0) {
+        return oldPheromoneValue;
+    } else {
+        float multiplicator = millisecondsSinceLastEvaporation / timeInterval;
+        float newPheromoneValue = oldPheromoneValue * pow(evaporationFactor, multiplicator);
+
+        if (newPheromoneValue < threshold) {
+            newPheromoneValue = 0;
+        }
+
+        return newPheromoneValue;
+    }
+}
