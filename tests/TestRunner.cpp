23a0430f55e788bcd95cf39b6a2688eff025f7d2
Friedrich Große <friedrich.grosse@gmail.com>
2013-03-27 10:26:09 +0100

    The Testrunner does now handle ARA:Exception

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -4,11 +4,18 @@
 
 #include "CppUTest/CommandLineTestRunner.h"
 #include "Environment.h"
+#include "Exception.h"
 #include "testAPI/mocks/time/ClockMock.h"
+#include <iostream>
 
 using namespace ARA;
 
 int main(int ac, char** av) {
-    Environment::setClock(new ClockMock());
-    return CommandLineTestRunner::RunAllTests(ac, av);
+    try {
+        Environment::setClock(new ClockMock());
+        return CommandLineTestRunner::RunAllTests(ac, av);
+    } catch(ARA::Exception& exception) {
+        std::cout << "\nERROR: Tests failed with ARA::Exception: " << exception.getMessage() << "\n\n";
+        return 1;
+    }
 }
