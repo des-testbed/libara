bafd37d590c88232d48590bc2e020dedf650be7f
Michael Frey <mail@mfrey.net>
2013-03-31 16:04:06 +0200

    added class OMNeTBestPheromoneForwardingPolicy in order to support the best pheromone forwarding policy in simulation

---
+++ omnetpp/forwarding/OMNeTBestPheromoneForwardingPolicy.cpp
@@ -0,0 +1,17 @@
+#include "omnetpp/OMNeTBestPheromoneForwardingPolicy.h"
+
+namespace ARA {
+    namespace omnetpp {
+
+        Define_Module(OMNeTBestPheromoneForwardingPolicy);
+
+        void OMNeTBestPheromoneForwardingPolicy::initialize(){
+
+        }
+
+        void OMNeTBestPheromoneForwardingPolicy::handleMessage(cMessage *msg) {
+            throw cRuntimeError("OMNeTBestPheromoneForwardingPolicy: handleMessage() should never be called!");
+        }
+
+    } /* namespace omnetpp */
+} /* namespace ARA */
