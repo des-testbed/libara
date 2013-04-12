9722c9c635e3eddaa9501a41b069b8da257cae71
Friedrich Große <friedrich.grosse@gmail.com>
2013-04-12 18:25:01 +0200

    Created a stochastic test for the StochasticForwardingPolicy
    Theoretically it could fail without beeing wrong.
    But the chances are negligible :)

--- tests/libara/core/forwarding/StochasticForwardingPolicyTest.cpp
+++ tests/libara/core/forwarding/StochasticForwardingPolicyTest.cpp
@@ -21,27 +21,76 @@ using namespace ARA;
 
 typedef std::shared_ptr<Address> AddressPtr;
 
-TEST_GROUP(StochasticForwardingPolicyTest) {};
+TEST_GROUP(StochasticForwardingPolicyTest) {
+    EvaporationPolicy* evaporationPolicy;
+    RoutingTable* routingTable;
+    NetworkInterfaceMock* interface;
+
+    void setup() {
+        evaporationPolicy = new ExponentialEvaporationPolicyMock();
+        routingTable = new RoutingTable();
+        routingTable->setEvaporationPolicy(evaporationPolicy);
+        interface = new NetworkInterfaceMock();
+    }
+
+    void teardown() {
+        delete routingTable;
+        delete evaporationPolicy;
+        delete interface;
+    }
+};
 
 TEST(StochasticForwardingPolicyTest, testGetNextHop) {
-    EvaporationPolicy* evaporationPolicy = new ExponentialEvaporationPolicyMock();
-    RoutingTable routingTable = RoutingTable();
-    routingTable.setEvaporationPolicy(evaporationPolicy);
-    AddressPtr destination (new AddressMock("Destination"));
-    NetworkInterfaceMock interface = NetworkInterfaceMock();
-
-    AddressPtr nextHopA (new AddressMock("nextHopA"));
-    AddressPtr nextHopB (new AddressMock("nextHopB"));
-
     PacketMock packet = PacketMock();
+    AddressPtr route1 (new AddressMock("A"));
+    AddressPtr route2 (new AddressMock("B"));
 
-    routingTable.update(destination, nextHopA, &interface, 1.2);
-    routingTable.update(destination, nextHopB, &interface, 2.1);
+    routingTable->update(packet.getDestination(), route1, interface, 1.2);
+    routingTable->update(packet.getDestination(), route2, interface, 2.1);
 
-    StochasticForwardingPolicyMock policy = StochasticForwardingPolicyMock(42);
+    unsigned int seed = 42;
+    StochasticForwardingPolicyMock policy = StochasticForwardingPolicyMock(seed);
 
-    NextHop* result = policy.getNextHop(&packet, &routingTable);
-    CHECK(result->getAddress()->equals(nextHopA));
+    NextHop* nextHop = policy.getNextHop(&packet, routingTable);
+    CHECK(nextHop->getAddress()->equals(route1));
+}
 
-    delete evaporationPolicy;
+TEST(StochasticForwardingPolicyTest, stochasticBehaviour) {
+    PacketMock packet = PacketMock();
+    AddressPtr route1 (new AddressMock("A"));
+    AddressPtr route2 (new AddressMock("B"));
+    AddressPtr route3 (new AddressMock("C"));
+
+    routingTable->update(packet.getDestination(), route1, interface, 3.0);
+    routingTable->update(packet.getDestination(), route2, interface, 6.0);
+    routingTable->update(packet.getDestination(), route3, interface, 1.0);
+
+    int nrOfIterations = 10000;
+    int nrOfTimesRoute1IsChosen = 0;
+    int nrOfTimesRoute2IsChosen = 0;
+    int nrOfTimesRoute3IsChosen = 0;
+
+    StochasticForwardingPolicyMock policy = StochasticForwardingPolicyMock();
+    for (int i = 0; i < nrOfIterations; i++) {
+        NextHop* nextHop = policy.getNextHop(&packet, routingTable);
+        if(nextHop->getAddress()->equals(route1)) {
+            nrOfTimesRoute1IsChosen++;
+        }
+        else if(nextHop->getAddress()->equals(route2)) {
+            nrOfTimesRoute2IsChosen++;
+        }
+        else if(nextHop->getAddress()->equals(route3)) {
+            nrOfTimesRoute3IsChosen++;
+        }
+    }
+
+    unsigned int allowedDeviation = nrOfIterations/20; // allow a deviation of +- 5%
+    CHECK(nrOfTimesRoute1IsChosen > (nrOfIterations*0.3 - allowedDeviation));
+    CHECK(nrOfTimesRoute1IsChosen < (nrOfIterations*0.3 + allowedDeviation));
+
+    CHECK(nrOfTimesRoute2IsChosen > (nrOfIterations*0.6 - allowedDeviation));
+    CHECK(nrOfTimesRoute2IsChosen < (nrOfIterations*0.6 + allowedDeviation));
+
+    CHECK(nrOfTimesRoute3IsChosen > (nrOfIterations*0.1 - allowedDeviation));
+    CHECK(nrOfTimesRoute3IsChosen < (nrOfIterations*0.1 + allowedDeviation));
 }
