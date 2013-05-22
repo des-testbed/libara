8e779bfb805947ab4670109a54d80fd101597937
Friedrich Große <friedrich.grosse@gmail.com>
2013-05-22 23:21:55 +0200

    The RoutingTableDataPersistor does now save the routing table data as binary to file

--- omnetpp/RoutingTableDataPersistor.cpp
+++ omnetpp/RoutingTableDataPersistor.cpp
@@ -2,53 +2,90 @@
  * $FU-Copyright$
  */
 
-#include "RoutingTableDataPersistor.h".h"
+#include "RoutingTableDataPersistor.h"
 #include "Environment.h"
+
+#include <unistd.h>
+#include <stdio.h>
+#include <endian.h>
 
 OMNETARA_NAMESPACE_BEGIN
 
 using namespace std;
 
 RoutingTableDataPersistor::RoutingTableDataPersistor(cModule* hostModule, int updateIntervallInMillis) {
-    const char* fileName = getFileName(hostModule);
     updateIntervall = updateIntervallInMillis;
-    file.open(fileName, ios::in | ios::out | ios::binary);
+    const char* fileName = getFileName(hostModule);
+    file.open(fileName, ios::out | ios::binary);
+    if(!file.good()) {
+        throw cRuntimeError("could not open file %s, to write routing table data", fileName);
+    }
 }
 
 RoutingTableDataPersistor::~RoutingTableDataPersistor() {
+    if(lastWriteTime != nullptr) {
+        delete lastWriteTime;
+    }
+
+    file.flush();
     file.close();
 }
 
 const char* RoutingTableDataPersistor::getFileName(cModule* hostModule) const {
     stringstream fileName;
-    fileName << hostModule->getFullName() << "-RoutingTable.bin";
+    cConfigurationEx* config = ev.getConfigEx();
+    fileName << "results/" << config->getActiveConfigName() << "-" << config->getActiveRunNumber() << "-" << hostModule->getName() << hostModule->getIndex() << "-RoutingTable.bin";
     return fileName.str().c_str();
 }
 
+/**
+ * Writes the current entries of the given RoutingTable to file if at least
+ * n milliseconds have passed since the last write (where n is the updateIntervall
+ * given the class constructor).
+ *
+ * The binary format of the written data is defined as follows:
+ * | 8 Byte timestamp | 1 Byte nrOfEntries | <entry_data> | ... | [<entry_data>]
+ *
+ * <entry_data> is defined as a triple in the following format:
+ * | 4 Byte destination | 4 Byte nextHop | 4 Byte pheromone value |
+ *
+ * The format stores all <entry_data> triples directly one after another.
+ *
+ * Note that all binary data is stored in big endian format
+ */
 void RoutingTableDataPersistor::write(RoutingTable* routingTable) {
     OMNeTTime* currentTime = dynamic_cast<OMNeTTime*>(Environment::getClock()->makeTime());
     currentTime->setToCurrentTime();
 
     if(lastWriteTime == nullptr
-    || lastWriteTime->getDifferenceInMilliSeconds(currentTime) >= updateIntervall) {
+    || currentTime->getDifferenceInMilliSeconds(lastWriteTime) >= updateIntervall) {
 
-        char nullByte = 0x00;
-        int64 rawTime = currentTime->getRawTime();
-        float pheromoneValue = 123.456;
-
+        int64 rawTime = htobe64(currentTime->getRawTime());
         file.write((char*)&rawTime, sizeof(rawTime));
-        int nrOfEntries = routingTable->getTotalNumberOfEntries();
+
+        int nrOfEntries = htole32(routingTable->getTotalNumberOfEntries());
+        file.write((char*)&nrOfEntries, 1);
+
         for (int i = 0; i < nrOfEntries; i++) {
             RoutingTableEntryTupel entryTupel = routingTable->getEntryAt(i);
-            AddressPtr destination = entryTupel.destination;
-            RoutingTableEntry* entry = entryTupel.entry;
+            OMNeTAddress* destination = dynamic_cast<OMNeTAddress*>(entryTupel.destination.get());
+            uint32 destinationInt = htobe32(destination->getInt());
 
-            //TODO write dest
-            //TODO write next
+            RoutingTableEntry* entry = entryTupel.entry;
+            OMNeTAddress* nextHop = dynamic_cast<OMNeTAddress*>(entry->getAddress().get());
+            uint32 nextHopInt = htobe32(nextHop->getInt());
+
+            float pheromoneValue = entry->getPheromoneValue();
+
+            file.write((char*)&destinationInt, 4);
+            file.write((char*)&nextHopInt, 4);
             file.write((char*)&pheromoneValue, sizeof(pheromoneValue));
         }
-        file.write((char*)&nullByte, 1);
 
+        // flush to file in case the simulation crashes or anything happens
+        file.flush();
+
+        delete lastWriteTime;
         lastWriteTime = currentTime;
     }
 }
