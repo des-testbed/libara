55efe490a4bf0817c44790af2154bdb9cc4838c0
Michael Frey <mail@mfrey.net>
2013-06-11 08:31:50 +0200

    added MeshHandler stub for TestbedProxy

---
+++ include/testbed/MeshHandler.h
@@ -0,0 +1,11 @@
+#ifndef _MESH_HANDLER_H_
+#define _MESH_HANDLER_H_
+
+#include "CLibs.h"
+
+class MeshHandler {
+    public:
+        virtual dessert_cb_result handler(dessert_msg_t* msg, size_t len, dessert_msg_proc_t* proc, dessert_meshif_t* iface_in, dessert_frameid_t id) = 0;
+};
+
+#endif 
