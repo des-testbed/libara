46b9e0a4f32b95246f2618a822152e37b4e8390c
Michael Frey <mail@mfrey.net>
2013-06-11 08:31:22 +0200

    added method stubs for testbed proxy (for handling callbacks)

---
+++ include/testbed/TimerHandler.h
@@ -0,0 +1,11 @@
+#ifndef _TIMER_HANDLER_H_
+#define _TIMER_HANDLER_H_
+
+#include "CLibs.h"
+
+class TimerHandler {
+    public:
+        virtual dessert_per_result_t handler(void* data, struct timeval* scheduled, struct timeval* interval) = 0;
+};
+
+#endif 
