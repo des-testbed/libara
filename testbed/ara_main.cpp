310a7939508cec058fabcbe516e4d4218f01bb1d
Grey Newell <ahteeim@gmail.com>
2013-06-17 11:16:39 +0200

    Wrote + properly registered command  testSendMesh

--- testbed/ara_main.cpp
+++ testbed/ara_main.cpp
@@ -15,6 +15,18 @@ _dessert_cb_results PacketToMeshDispatcher (dessert_msg_t* ReceivedMessage, uint
 
 _dessert_cb_results PacketToSystemDispatcher (dessert_msg_t* ReceivedMessage, uint32_t Length, dessert_msg_proc_t *ProcessingFlags, const dessert_meshif_t *MeshInterface, dessert_frameid_t id);
 
+int testbed_cli_cmd_testsendmesh(struct cli_def* cli, char* command, char* argv[], int argc){
+    dessert_msg_t *TestPacket = NULL;
+    dessert_msg_new(&TestPacket);
+    if(TestPacket != NULL) {
+        dessert_syssend_msg(TestPacket);
+        return 0;
+    }
+    else {
+        return 1;
+    }
+
+}
 
 
 int main(int argc, char** argv) {
@@ -29,10 +41,10 @@ int main(int argc, char** argv) {
      dessert_logcfg(DESSERT_LOG_STDERR | DESSERT_LOG_GZ); // enable compression, maybe this helps
      cli_register_command(dessert_cli, dessert_cli_cfg_iface, const_cast<char*>("sys"), dessert_cli_cmd_addsysif, PRIVILEGE_PRIVILEGED, MODE_CONFIG, const_cast<char*>("initialize tap interface"));
      cli_register_command(dessert_cli, dessert_cli_cfg_iface, const_cast<char*>("mesh"), dessert_cli_cmd_addmeshif, PRIVILEGE_PRIVILEGED, MODE_CONFIG, const_cast<char*>("initialize mesh interface"));
+     cli_register_command(dessert_cli, dessert_cli_show, const_cast<char*>("testSendMesh"), testbed_cli_cmd_testsendmesh, PRIVILEGE_UNPRIVILEGED, MODE_ANY, const_cast<char*>("send a test packet to mesh interface"));
 
      _dessert_cb_results (*ToMesh)(dessert_msg_t*, uint32_t, dessert_msg_proc_t*, dessert_sysif_t*, dessert_frameid_t) = &PacketToMeshDispatcher;
      dessert_sysrxcb_add(ToMesh, 50);
-
 
      dessert_debug("applying configuration");
      cli_file(dessert_cli, cfg, PRIVILEGE_PRIVILEGED, MODE_CONFIG);
