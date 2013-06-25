c4393c72d7dec1f16fbd7f314eadc1ae0cc8a1c2
Grey Newell <ahteeim@gmail.com>
2013-06-25 14:16:40 +0200

    TestbedAddressTest equals, gethashvalue, tostring

--- tests/testbed/TestbedAddressTest.cpp
+++ tests/testbed/TestbedAddressTest.cpp
@@ -12,7 +12,24 @@ TEST_GROUP(TestbedAddressTest) {};
 
 TEST(TestbedAddressTest, testToString) {
     TestbedAddress address(1,2,3,4,5,6);
-    std::cout << address.toString() << std::endl;
+    std::cout << "Address 1:2:3:4:5:6 "<< address.toString() << std::endl;
+}
+
+TEST(TestbedAddressTest, testGetHashValue) {
+    TestbedAddress address(1,2,3,4,5,6);
+    std::cout << "Hash of address "<< address.getHashValue() << std::endl;
+}
+
+TEST(TestbedAddressTest, testEquals) {
+    TestbedAddress address1(6,5,4,3,2,1);
+    TestbedAddress address2(6,5,4,3,2,1);
+    CHECK_TRUE(address1==address2);
+}
+
+TEST(TestbedAddressTest, testNotEquals) {
+    TestbedAddress address1(6,5,4,3,2,1);
+    TestbedAddress address2(1,2,3,4,5,6);
+    CHECK_FALSE(address1==address2);
 }
 
 TESTBED_NAMESPACE_END
