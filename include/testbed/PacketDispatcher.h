b7957039369dc61dbc23539fea0281e20e194c89
Grey Newell <ahteeim@gmail.com>
2013-06-26 15:04:25 +0200

    INCOMPLETE PacketDispatcher test + implementation

--- include/testbed/PacketDispatcher.h
+++ include/testbed/PacketDispatcher.h
@@ -6,8 +6,18 @@
 #define PACKET_DISPATCHER_H
 
 #include "CLibs.h"
+#include "Testbed.h"
+#include "Packet.h"
+
+TESTBED_NAMESPACE_BEGIN
+
+#define DESSERT_LOCAL_ADDRESS dessert_l25_defsrc
+#define DESSERT_BROADCAST_ADDRESS ether_broadcast
 
 _dessert_cb_results PacketToMeshDispatcher (dessert_msg_t* ReceivedMessage, uint32_t Length, dessert_msg_proc_t *ProcessingFlags, dessert_sysif_t *SystemInterface, dessert_frameid_t id);
 _dessert_cb_results PacketToSystemDispatcher (dessert_msg_t* ReceivedMessage, uint32_t Length, dessert_msg_proc_t *ProcessingFlags, dessert_meshif_t *MeshInterface, dessert_frameid_t id);
+Packet* extractPacket(dessert_msg_t* dessertMessage);
+
+TESTBED_NAMESPACE_END
 
 #endif /*PACKET_DISPATCHER_H*/
--- testbed/PacketDispatcher.cpp
+++ testbed/PacketDispatcher.cpp
@@ -4,6 +4,8 @@
 
 #include "PacketDispatcher.h"
 
+TESTBED_NAMESPACE_BEGIN
+
 _dessert_cb_results PacketToMeshDispatcher (dessert_msg_t* ReceivedMessage, uint32_t Length, dessert_msg_proc_t* ProcessingFlags, dessert_sysif_t* SystemInterface, dessert_frameid_t id) {
     dessert_debug("PacketToMeshDispatcher: Received packet from sys, broadcasting via mesh");
 
@@ -26,7 +28,20 @@ _dessert_cb_results PacketToSystemDispatcher (dessert_msg_t* ReceivedMessage, ui
             eth_len = dessert_msg_ethdecap(ReceivedMessage, &eth);
             dessert_syssend(eth, eth_len);
             free(eth);
-  }
+    }
 
-  return DESSERT_MSG_KEEP;
+    return DESSERT_MSG_KEEP;
 }
+
+Packet* extractPacket(dessert_msg_t* dessertMessage) {
+    AddressPtr source;
+    AddressPtr destination;
+    AddressPtr sender;
+    char type = 0x01;
+    unsigned int sequenceNumber = dessertMessage->u16;
+    int ttl = dessertMessage->ttl;
+
+    return new Packet(source, destination, sender, type, sequenceNumber, ttl);
+}
+
+TESTBED_NAMESPACE_END
