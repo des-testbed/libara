99afac54f7c603601550cdb0c4a3310bc295f259
Michael Frey <frey@inf.fu-berlin.de>
2013-07-01 17:50:40 +0200

    added stub for class TestbedTimer

---
+++ testbed/TestbedTimer.cpp
@@ -0,0 +1,17 @@
+#include "TestbedTimer.h"
+
+TESTBED_NAMESPACE_BEGIN
+
+TestbedTimer::TestbedTimer(){}
+
+TestbedTimer::~TestbedTimer(){}
+
+void TestbedTimer::run(unsigned long timeoutInMicroSeconds){
+   // typedef std::chrono::duration<int,std::milli>
+}
+
+void TestbedTimer::interrupt(){
+
+}
+
+TESTBED_NAMESPACE_END
