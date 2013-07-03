c92530b029d69db2cde50f0396c4875680366660
Grey Newell <ahteeim@gmail.com>
2013-07-03 14:54:01 +0200

    Removed legacy comments, now inaccurate

--- testbed/TestbedMain.cpp
+++ testbed/TestbedMain.cpp
@@ -80,9 +80,7 @@ int main(int argc, char** argv) {
      cli_register_command(dessert_cli, dessert_cli_cfg_iface, const_cast<char*>("mesh"), dessert_cli_cmd_addmeshif, PRIVILEGE_PRIVILEGED, MODE_CONFIG, const_cast<char*>("initialize mesh interface"));
      cli_register_command(dessert_cli, dessert_cli_show, const_cast<char*>("testSendMesh"), testbed_cli_cmd_testsendmesh, PRIVILEGE_UNPRIVILEGED, MODE_ANY, const_cast<char*>("send a test packet to mesh interface"));
 
-     /// ara_tun2ara
      _dessert_cb_results (*fromNetwork)(dessert_msg_t*, uint32_t, dessert_msg_proc_t*, dessert_sysif_t*, dessert_frameid_t) = &ARA::testbed::messageFromNetworkDispatcher;
-     /// ara_ara2tun
      dessert_cb_result (*toNetwork)(dessert_msg_t*, uint32_t, dessert_msg_proc_t*, dessert_meshif_t*, dessert_frameid_t) = &ARA::testbed::messageToNetworkDispatcher;
 
      dessert_sysrxcb_add(toNetwork, 50);
