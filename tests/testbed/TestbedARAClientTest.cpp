699ca7bbd3a20b91cc8331654a75cd8808c563b0
Grey Newell <ahteeim@gmail.com>
2013-07-05 17:28:35 +0200

    Added instantiation test for TestbedARAClient

--- tests/testbed/TestbedARAClientTest.cpp
+++ tests/testbed/TestbedARAClientTest.cpp
@@ -22,8 +22,12 @@ TEST_GROUP(TestbedARAClientTest) {
     }
 };
 
-TEST(TestbedARAClientTest, equals) {
-    CHECK(1==1);
+/**
+ * Test checks instantiation, if the test compiles you may instantiate a TestbedARAClient.
+ * Checks for memory leaks in general use of client.
+ */
+TEST(TestbedARAClientTest, instantiation) {
+    TestbedARAClient client = TestbedARAClient();
 }
 
 TESTBED_NAMESPACE_END
