2ccfcaff21f9d1d2cb766110d1667db38d7fd789
Friedrich Große <friedrich.grosse@gmail.com>
2013-07-12 16:43:19 +0200

    If a batteries energy is depleted will will now record the 0 energy in the vector file

--- omnetpp/OMNeTBattery.cpp
+++ omnetpp/OMNeTBattery.cpp
@@ -124,6 +124,7 @@ void OMNeTBattery::handleMessage(cMessage* message) {
 void OMNeTBattery::updateResidualEnergy(){
     if (residualCapacityInMilliWattSeconds <= 0) {
         // The battery is already depleted and devices should have stopped sending drawMsg. However leftover messages in queue are caught
+        residualEnergyOutVector.record(0);
         return;
     }
 
