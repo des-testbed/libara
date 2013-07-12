7c40cfba7e5f742157e07c0abfeff281f1d91fdc
Friedrich Große <friedrich.grosse@gmail.com>
2013-07-12 19:23:41 +0200

    The TrafficGenerator does now stop to send traffic if the source nodes energy is depleted

--- include/omnetpp/TrafficGenerator.h
+++ include/omnetpp/TrafficGenerator.h
@@ -7,6 +7,7 @@
 
 #include "OMNeTARAMacros.h"
 #include "TrafGen.h"
+#include "INotifiable.h"
 #include "IPv4Address.h"
 #include "IPv4Datagram.h"
 #include "IPv4ControlInfo.h"
@@ -17,15 +18,18 @@ OMNETARA_NAMESPACE_BEGIN
  * The class provides a traffic generator for the ARA/EARA routing
  * protocols. It is based on the INETMANET class TrafGen.
  */
-class TrafficGenerator : public TrafGen {
+class TrafficGenerator : public TrafGen, public INotifiable {
     public:
         virtual void initialize(int level);
+        virtual void receiveChangeNotification(int category, const cObject* details);
 
     protected:
+        virtual void handleSelfMsg(cMessage* message);
         virtual void SendTraf(cPacket* msg, const char* destination);
         void sendTraffic(cPacket* message, const char* destination);
 
     private:
+        bool isEnergyDepleted;
         int nrOfPacketsToSend;
         int nrOfSentMessages = 0;
 };
