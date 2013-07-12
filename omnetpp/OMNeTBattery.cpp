07ce5c8037a101473df0ce96eaedc059d824b3c1
Friedrich Große <friedrich.grosse@gmail.com>
2013-07-12 17:33:13 +0200

    Revert "Fixed major bug in OMNeTBattery"
    This reverts commit bc91f71ae2402e5f01133cbc1c89d6d380244a31.
    This did not remove any bug

--- omnetpp/OMNeTBattery.cpp
+++ omnetpp/OMNeTBattery.cpp
@@ -122,6 +122,12 @@ void OMNeTBattery::handleMessage(cMessage* message) {
 }
 
 void OMNeTBattery::updateResidualEnergy(){
+    if (residualCapacityInMilliWattSeconds <= 0) {
+        // The battery is already depleted and devices should have stopped sending drawMsg. However leftover messages in queue are caught
+        residualEnergyOutVector.record(0);
+        return;
+    }
+
     calculateConsumedEnergy();
     publishEnergyInformation();
     updateBatteryIcon();
