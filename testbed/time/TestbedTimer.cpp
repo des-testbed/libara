2e117fe557d72c24fc15142b35e615fb9e6c9381
Grey Newell <ahteeim@gmail.com>
2013-07-29 14:52:47 +0200

    Implemented Workaround to timer issues: needs correction

--- testbed/time/TestbedTimer.cpp
+++ testbed/time/TestbedTimer.cpp
@@ -1,6 +1,8 @@
 #include "TestbedTimer.h"
 
 #include <iostream>
+#include <system_error>
+
 
 TESTBED_NAMESPACE_BEGIN
 
@@ -11,27 +13,41 @@ TestbedTimer::TestbedTimer(char type, void* contextObject) : Timer(type, context
 TestbedTimer::~TestbedTimer(){
     if (this->active) {
         this->active = false;
-        this->timer.join();
+        std::cout << "destructor has been called" << std::endl;
+/*
+        if(this->timer->joinable()){
+            std::cout << "thread is joinable" << std::endl;
+            try {
+                this->timer->join();
+            } catch (const std::system_error& e) {
+                std::cout << "Caught system_error with code " << e.code()  << " meaning " << e.what() << '\n';
+            }
+            std::cout << "thread has rejoined main scope" << std::endl;
+        }else{
+            std::cout << "no need to join" << std::endl;
+        }
+*/
+       // FIX IT!!!
+       // delete timer;
     }
 }
 
 void TestbedTimer::run(unsigned long timeoutInMicroSeconds){
     this->active = true;
-    timer = std::thread(&TestbedTimer::sleep, this, timeoutInMicroSeconds);
+    timer = new std::thread(&TestbedTimer::sleep, this, timeoutInMicroSeconds);
+
 }
 
 void TestbedTimer::sleep(unsigned long timeoutInMicroSeconds){
     int interval = 500000;
 
-    while(timeoutInMicroSeconds > 0){
+    while((timeoutInMicroSeconds > 0) && (active == true)){
         if (active) {
             timeoutInMicroSeconds -= interval;
             /// set the sleep time
             std::chrono::microseconds duration(interval);
             /// set thread to sleep
             std::this_thread::sleep_for(duration);
-        } else {
-            break;
         }
     }
 
@@ -42,7 +58,10 @@ void TestbedTimer::sleep(unsigned long timeoutInMicroSeconds){
 
 void TestbedTimer::interrupt(){
     this->active = false;
-    this->timer.join();
+    //std::cout << "interrupt has been called" << std::endl;
+    this->timer->join();
+    //std::cout << "thread has rejoined main scope" << std::endl;
+    //delete timer;
 }
 
 TESTBED_NAMESPACE_END
