ed6edcb02eedfc2c7214bd59e8824ba6e7afd5f1
Michael Frey <frey@inf.fu-berlin.de>
2014-05-07 18:23:39 +0200

    added todo note on issue with va_list in class SimpleLogger

--- src/core/SimpleLogger.cpp
+++ src/core/SimpleLogger.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 namespace ARA {
 
+
+// TODO: we should fix this since 'va_start' has undefined behavior with reference types
 void SimpleLogger::performLoggingAction(const std::string &logMessage, Level level, va_list args) const {
     char buffer[512]; // FIXME a dynamic buffer size would be awesome
     vsprintf(buffer, logMessage.c_str(), args);
