e8bdc58eedb4e825df9eec4eeda6c863311ee145
Michael Frey <frey@inf.fu-berlin.de>
2014-05-18 14:30:01 +0200

    fixed indentation in class StndardClock

--- src/time/StandardClock.cpp
+++ src/time/StandardClock.cpp
@@ -14,12 +14,12 @@ Time* StandardClock::makeTime(){
 }
 
 TimerPtr StandardClock::getNewTimer(TimerType timerType, void* contextObject){
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     /// create a proxy for the standard timer	    
     std::shared_ptr<StandardTimerProxy> result = std::make_shared<StandardTimerProxy>(timerType, contextObject);
     /// set the identifier of the timer
     result->setTimerIdentifier(timerList.size());
-    /// create a standard timer and append it to the list
+    /// create a standard timer
     std::shared_ptr<StandardTimer> timer = std::make_shared<StandardTimer>(timerType);
     /// create a weak pointer to the proxy timer 
     std::weak_ptr<StandardTimerProxy> callback = result;
@@ -32,25 +32,26 @@ TimerPtr StandardClock::getNewTimer(TimerType timerType, void* contextObject){
 }
 
 void StandardClock::scheduleTimer(unsigned long identifier, unsigned long timeoutInMicroseconds){
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
+
     try {
-	std::shared_ptr<StandardTimer> timer = timerList.at(identifier); 
-	timer->run(timeoutInMicroseconds);
+        std::shared_ptr<StandardTimer> timer = timerList[identifier]; 
+        timer->run(timeoutInMicroseconds);
     } catch (const std::out_of_range error) {
         std::cerr << "out of bound exception in class StandardClock " << error.what() << std::endl;
     }	    
 }
 
 void StandardClock::scheduleTimer(std::function<void()> timer){
-    std::unique_lock<std::mutex> lock(mutex);
     threadPool.schedule(timer);
 }
 
 void StandardClock::interruptTimer(unsigned long identifier){
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
+
     try {
-	std::shared_ptr<StandardTimer> timer = timerList.at(identifier); 
-	timer->interrupt();
+        std::shared_ptr<StandardTimer> timer = timerList[identifier]; 
+        timer->interrupt();
     } catch (const std::out_of_range error) {
         std::cerr << "out of bound exception in class StandardClock " << error.what() << std::endl;
     }	    
