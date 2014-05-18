631a0b0fd7a82f79575927364f775e2b5b9cdff8
Michael Frey <frey@inf.fu-berlin.de>
2014-05-18 13:59:45 +0200

    removed unimplemented functions from header in TestbedPacketDispatcher, renamed functions for TestbedMain

--- include/testbed/TestbedPacketDispatcher.h
+++ include/testbed/TestbedPacketDispatcher.h
@@ -10,77 +10,36 @@
 #include "Packet.h"
 #include "testbed/TestbedNetworkInterface.h"
 #include "TestbedARAClient.h"
+#include "TestbedPacketFactory.h"
 #include <unordered_map>
 
 TESTBED_NAMESPACE_BEGIN
 
-typedef std::unordered_map<dessert_meshif_t*, TestbedNetworkInterface*> NetworkInterfaceMap; //instantiated in .cpp file
-
-struct routingExtension {
-    u_int8_t  ara_dhost[ETH_ALEN];  /* ARA destination eth addr */
-    u_int8_t  ara_shost[ETH_ALEN];  /* ARA source ether addr    */
-    u_int16_t ara_data;             /* reserved for future ARA use */
-} __attribute__ ((__packed__));
-
 /**
  * The first ara callback of the mesh processing pipeline, determines if a packet belongs to libARA and sends to araMessageDispatcher, or the kernel, accordingly.
  */
-_dessert_cb_results messageFromMeshInterfaceDispatcher(dessert_msg_t* messageReceived, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_meshif_t* interface, dessert_frameid_t id);
-
-/**
- * Determines if a given dessert message was created by TestbedARA.
- */
-bool isARAMessage(dessert_msg_t* message);
-
-/**
- * The second ara callback of the mesh processing pipeline, extracts a packet from the dessert message and sends to libARA.
- */
-_dessert_cb_results araMessageDispatcher(dessert_msg_t* messageReceived, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_meshif_t* interface, dessert_frameid_t id);
+dessert_cb_result toSys(dessert_msg_t* messageReceived, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_meshif_t* interface, dessert_frameid_t id);
 
 /**
  * Receives a Packet from an Interface and converts to a message, then sends it over selected interface (if NULL sends over all interfaces)
  */
-void packetToMeshInterfaceDispatcher(const Packet* packet, TestbedNetworkInterface* testbedInterface, std::shared_ptr<Address> recipient);
+int _send(const Packet* packet, std::shared_ptr<TestbedAddress> interfaceAddress, std::shared_ptr<Address> recipient);
 
 /**
- * Extracts all data from a dessert message that is necessary to create a libARA Packet object.
+ *
  */
-Packet* extractPacket(dessert_msg_t* dessertMessage);
+dessert_cb_result toMesh(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *flags, dessert_sysif_t *interface, dessert_frameid_t id);
 
 /**
- * Creates a packet from a message received from the system.
+ *
  */
-Packet* tapMessageToPacket(dessert_msg_t* dessertMessage, std::weak_ptr<TestbedARAClient> testbedClient);
+void dumpDessertMessage(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *flags, dessert_sysif_t *interface, dessert_frameid_t id);
 
 /**
- * Extracts the ethernet header struct from the given dessert message.
+ *
  */
-ether_header* extractEthernetHeader(dessert_msg_t* dessertMessage);
+dessert_cb_result packetFilter(dessert_msg_t* messageReceived, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_sysif_t *interface, dessert_frameid_t id);
 
-/**
- * Extracts the libARA routingExtension from the given dessert message.
- */
-routingExtension* extractRoutingExtension(dessert_msg_t* dessertMessage);
-
-/**
- * Extracts all data from a packet and stores in a dessert message.
- */
-dessert_msg_t* extractDessertMessage(const Packet* packet);
-
-/**
- * Adds a ethernet header to a dessert message.
- */
-void addEthernetHeader(dessert_msg_t* message, dessert_meshif_t* interface, std::shared_ptr<Address> nextHop);
-
-/**
- * Adds an ARA routing extension to a dessert message.
- */
-void addRoutingExtension(dessert_msg_t* message, u_int8_t* source, u_int8_t* destination);
-
-/**
- * Returns a pointer to the testbed network interface paired with a given dessert meshinterface.
- */
-TestbedNetworkInterface* extractNetworkInterface(dessert_meshif_t* dessertInterface);
 
 TESTBED_NAMESPACE_END
 
--- testbed/TestbedPacketDispatcher.cpp
+++ testbed/TestbedPacketDispatcher.cpp
@@ -2,160 +2,78 @@
  * $FU-Copyright$
  */
 
+#include "TestbedPacket.h"
 #include "TestbedPacketDispatcher.h"
 #include "TestbedAddress.h"
 
 #include <netinet/in.h>
 
+extern std::shared_ptr<ARA::testbed::TestbedARAClient> client;
+
 TESTBED_NAMESPACE_BEGIN
 
 /**
- * Stores a map of network interface pointers by dessert interface pointers.
+ * RECEIVE
  */
-NetworkInterfaceMap networkInterfaces;
+dessert_cb_result toSys(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_meshif_t* interface, dessert_frameid_t id) {
+    TestbedPacketFactory* packetFactory = dynamic_cast<TestbedPacketFactory*>(client->getPacketFactory());
+    // DEBUG: std::cout << "[messageFromMeshInterfaceDispatcher] heyho i've got a packet" << std::endl;
+    TestbedPacket* packet = packetFactory->makeNewPacket(message);
+    // DEBUG: std::cout << " Packet Dump " << std::endl;
+    // DEBUG: std::cout << packet;
 
-_dessert_cb_results messageFromMeshInterfaceDispatcher(dessert_msg_t* messageReceived, uint32_t length, dessert_msg_proc_t *processingFlags, dessert_meshif_t* interface, dessert_frameid_t id) {
-    /// DEBUG: 
-    std::cout << "[messageFromMeshInterfaceDispatcher] heyho i've got a packet" << std::endl;
-    Packet* packet = extractPacket(messageReceived);
-    std::cout << " Packet Dump " << std::endl;
-    std::cout << packet->toString() << std::endl;
-    TestbedNetworkInterface* networkInterface = extractNetworkInterface(interface);
+    TestbedNetworkInterface* networkInterface = client->getTestbedNetworkInterface(std::make_shared<TestbedAddress>(interface->hwaddr));
     networkInterface->receive(packet);
+
     return DESSERT_MSG_DROP;
 }
 
-void packetToMeshInterfaceDispatcher(const Packet* packet, TestbedNetworkInterface* testbedInterface, std::shared_ptr<Address> recipient) {
-    // DEBUG: 
-    std::cout << "[packetToMeshInterfaceDispatcher] heyho i've got a packet" << std::endl;
-    dessert_msg_t* message = extractDessertMessage(packet);
-    dessert_meshif_t* interface = testbedInterface->getDessertPointer();
-    addEthernetHeader(message, interface, recipient);
+/**
+ * SEND
+ */
+int _send(const Packet* packet, std::shared_ptr<TestbedAddress> interfaceAddress, std::shared_ptr<Address> recipient) {
+    TestbedPacketFactory* packetFactory = dynamic_cast<TestbedPacketFactory*>(client->getPacketFactory());
+    /// determine the mesh interface
+    dessert_meshif_t* interface = dessert_meshif_get_hwaddr(interfaceAddress->getDessertValue());
+    //
+    dessert_msg_t* message = packetFactory->makeDessertMessage(packet, interface, recipient);
+    ///
     dessert_meshsend(message, interface);
+    ///
+    return DESSERT_MSG_DROP;
 }
 
-Packet* extractPacket(dessert_msg_t* dessertMessage) {
-    ether_header* ethernetFrame = extractEthernetHeader(dessertMessage);
-    routingExtension* araHeader = extractRoutingExtension(dessertMessage);
 
-    AddressPtr source = std::make_shared<TestbedAddress>(araHeader->ara_shost);
-    AddressPtr destination = std::make_shared<TestbedAddress>(araHeader->ara_dhost);
-    AddressPtr sender = std::make_shared<TestbedAddress>(ethernetFrame->ether_shost);
+dessert_cb_result toMesh(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *flags, dessert_sysif_t *interface, dessert_frameid_t id) {
+    dumpDessertMessage(message, length, flags, interface, id);
 
-    char packetType = dessertMessage->u8;
-    unsigned int sequenceNumber = dessertMessage->u16;
-    int ttl = dessertMessage->ttl;
-
-    void* payload;
-    unsigned int payloadSize = ntohs(dessert_msg_getpayload(dessertMessage, &payload));
-    std::cout << "[extractPacket] payload length is " <<  payloadSize << std::endl;
-
-    if (payloadSize == 0){
-        payload = nullptr;
-    }
-
-    return new Packet(source, destination, sender, packetType, sequenceNumber, ttl, (const char*)payload, payloadSize);
+    TestbedPacketFactory* packetFactory = dynamic_cast<TestbedPacketFactory*>(client->getPacketFactory());
+    // DEBUG: std::cout << "[messageFromMeshInterfaceDispatcher] heyho i've got a packet" << std::endl;
+    TestbedPacket* packet = packetFactory->makeDataPacket(message);
+    std::cout << "Packet Dump:" << std::endl;
+    std::cout << *packet << std::endl;
+    client->sendPacket(packet);
+    return DESSERT_MSG_DROP;
 }
 
-Packet* tapMessageToPacket(dessert_msg_t* dessertMessage, std::weak_ptr<TestbedARAClient> testbedClient) {
-    auto client = testbedClient.lock();
-    ether_header* ethernetFrame = extractEthernetHeader(dessertMessage);
-
-    AddressPtr source = std::dynamic_pointer_cast<Address>(std::make_shared<TestbedAddress>(ethernetFrame->ether_shost));
-    AddressPtr destination = std::dynamic_pointer_cast<Address>(std::make_shared<TestbedAddress>(ethernetFrame->ether_dhost));
-    std::cout << "[tapMessageToPacket] create packet from dessert message received over tap interface" << std::endl;
-    std::cout << "[tapMessageToPacket] source: " << source->toString() << " destination: " << destination->toString() << std::endl;
-
-    void* payload;
-    unsigned int payloadSize = ntohs(dessert_msg_getpayload(dessertMessage, &payload));
-    std::cout << "[tapMessageToPacket] payload length is " <<  payloadSize << std::endl;
-    if (payloadSize == 0){
-        payload = nullptr;
-    }
-
-    return client->getPacketFactory()->makeDataPacket(source, destination, client->getNextSequenceNumber(), (const char*)payload, payloadSize);
+void dumpDessertMessage(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *flags, dessert_sysif_t *interface, dessert_frameid_t id) {
+    char buffer[1024];
+    dessert_msg_proc_dump(message, length, flags, buffer, 1024);
+    std::cout << "received frame " << ((unsigned long)id) << " on interface " << interface->if_name << std::endl;
+    std::cout << buffer;
+    std::cout << std::endl;
 }
 
-ether_header* extractEthernetHeader(dessert_msg_t* dessertMessage) {
+dessert_cb_result packetFilter(dessert_msg_t* message, uint32_t length, dessert_msg_proc_t *flags, dessert_sysif_t *interface, dessert_frameid_t id) {
     dessert_ext_t* extension;
-    dessert_msg_getext(dessertMessage, &extension, DESSERT_EXT_ETH, 0);
-    return (ether_header*) extension->data;
-}
+    dessert_msg_getext(message, &extension, DESSERT_EXT_ETH, 0);
+    ether_header* ethernetFrame = (ether_header*) extension->data;
 
-routingExtension* extractRoutingExtension(dessert_msg_t* dessertMessage) {
-    dessert_ext_t* extension;
-
-    if(dessert_msg_getext(dessertMessage, &extension, DESSERT_EXT_USER, 0) == 0){
-        return nullptr;
-    }
-
-    return (routingExtension*) extension->data;
-}
-
-dessert_msg_t* extractDessertMessage(const Packet* packet) {
-    dessert_msg_t* dessertMessage;
-    dessert_msg_new(&dessertMessage);
-
-    dessertMessage->u16 = packet->getSequenceNumber();
-    dessertMessage->ttl = packet->getTTL();
-    dessertMessage->u8  = packet->getType();
-   
-    std::cout << "[extractDessertMessage] use count of source shared_ptr: " << packet->getSource().use_count() << std::endl; 
-    std::cout << "[extractDessertMessage] use count of destination shared_ptr: " << packet->getDestination().use_count() << std::endl; 
-
-    TestbedAddressPtr source = std::dynamic_pointer_cast<TestbedAddress>(packet->getSource());
-    std::cout << "[extractDessertMessage] use count of source shared_ptr after cast: " << source.use_count() << std::endl; 
-    TestbedAddressPtr destination = std::dynamic_pointer_cast<TestbedAddress>(packet->getDestination());
-    std::cout << "[extractDessertMessage] use count of destination shared_ptr after cast: " << destination.use_count() << std::endl; 
-
-    addRoutingExtension(dessertMessage, source->getDessertValue(), destination->getDessertValue());
-
-    //std::cout << "||Extract DES-SERT|| source: " << sourceTestbedAddress.get()->toString() << " destination: " << destinationTestbedAddress.get()->toString() << " sender: " << packet->getSenderString() << std::endl;
-
-    void* payload;
-    int payloadSize = packet->getPayloadLength();
-    std::cout << "[extractDessertMessage] payload length is " <<  packet->getPayloadLength() << std::endl;
-
-    if (dessert_msg_addpayload(dessertMessage, &payload, payloadSize) == DESSERT_OK){
-        memcpy(payload, packet->getPayload(), payloadSize);
-        //std::copy(packet->getPayload(), packet->getPayload() + payloadSize, payload);
+    if(ethernetFrame->ether_dhost[0] == 51) {
+        return DESSERT_MSG_DROP;
     } else {
-	std::cerr << "[extractDessertMessage] error on adding payload to dessert message " << std::endl;
+        return DESSERT_MSG_KEEP;
     }
-
-    return dessertMessage;
-}
-
-void addEthernetHeader(dessert_msg_t* message, dessert_meshif_t* interface, AddressPtr nextHop) {
-    dessert_ext_t* extension;
-    dessert_msg_addext(message, &extension, DESSERT_EXT_ETH, ETHER_HDR_LEN);
-
-    struct ether_header* ethernetFrame = (struct ether_header*) extension->data;
-    u_int8_t* senderMac = interface->hwaddr;
-    TestbedAddressPtr recipient = std::dynamic_pointer_cast<TestbedAddress>(nextHop);
-    u_int8_t* nextHopMac = recipient->getDessertValue();
-
-    std::copy(senderMac, senderMac + ETHER_ADDR_LEN, ethernetFrame->ether_shost);
-    std::copy(nextHopMac, nextHopMac + ETHER_ADDR_LEN, ethernetFrame->ether_dhost);
-}
-
-void addRoutingExtension(dessert_msg_t* message, u_int8_t* source, u_int8_t* destination) {
-    dessert_ext_t* extension;
-    dessert_msg_addext(message, &extension, DESSERT_EXT_USER, ETHER_HDR_LEN);
-    struct routingExtension* araRoutingExtension = (struct routingExtension*) extension->data;
-
-    std::copy(source, source + ETHER_ADDR_LEN, araRoutingExtension->ara_shost);
-    std::copy(destination, destination + ETHER_ADDR_LEN, araRoutingExtension->ara_dhost);
-}
-
-TestbedNetworkInterface* extractNetworkInterface(dessert_meshif_t* dessertInterface) {
-    std::unordered_map<dessert_meshif_t*, TestbedNetworkInterface*>::const_iterator got = networkInterfaces.find(dessertInterface);
-
-    if (got == networkInterfaces.end()) {
-        return nullptr;
-    } 
-
-    return got->second;
 }
 
 TESTBED_NAMESPACE_END
