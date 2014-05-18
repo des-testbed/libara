8e738cee82d78ebb8bf33890a3c1ef72eb18286b
Michael Frey <frey@inf.fu-berlin.de>
2014-05-18 14:19:15 +0200

    changed unique_locak to lock_guard in class TestbedRoutingTable

--- testbed/TestbedRoutingTable.cpp
+++ testbed/TestbedRoutingTable.cpp
@@ -14,61 +14,54 @@ void TestbedRoutingTable::update(AddressPtr destination, RoutingTableEntry* newE
     RoutingTable::update(destination, newEntry);
 }
 
-/*
-void TestbedRoutingTable::applyEvaporation(Time* currentTime) {
-    std::lock_guard<std::mutex> lock(mutex);
-    RoutingTable::applyEvaporation(currentTime);
-}
-*/
-
 float TestbedRoutingTable::getPheromoneValue(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     float result = RoutingTable::getPheromoneValue(destination, nextHop, interface);
     return result;
 }
 
 void TestbedRoutingTable::removeEntry(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     RoutingTable::removeEntry(destination, nextHop, interface);
 }
 
 RoutingTableEntryList TestbedRoutingTable::getPossibleNextHops(const Packet* packet) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     RoutingTableEntryList result = RoutingTable::getPossibleNextHops(packet);
     return result;
 }
 
 RoutingTableEntryList TestbedRoutingTable::getPossibleNextHops(AddressPtr destination) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     RoutingTableEntryList result = RoutingTable::getPossibleNextHops(destination);
     return result;
 }
 
 bool TestbedRoutingTable::isDeliverable(AddressPtr destination) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     bool result = RoutingTable::isDeliverable(destination);
     return result;
 }
 
 bool TestbedRoutingTable::isDeliverable(const Packet* packet) {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     bool result = RoutingTable::isDeliverable(packet);
     return result;
 }
 
 bool TestbedRoutingTable::exists(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface){
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     bool result = RoutingTable::exists(destination, nextHop, interface);
     return result;
 }
-
+/*
 void TestbedRoutingTable::triggerEvaporation() {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     RoutingTable::triggerEvaporation();
 }
-
+*/
 unsigned int TestbedRoutingTable::getTotalNumberOfEntries() const {
-    std::unique_lock<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(mutex);
     unsigned int result = RoutingTable::getTotalNumberOfEntries();
     return result;
 }
@@ -82,6 +75,12 @@ std::deque<RoutingTableEntryTupel> TestbedRoutingTable::getAllRoutesThatLeadOver
 RoutingTableEntryTupel TestbedRoutingTable::getEntryAt(int wantedPosition) const {
     std::lock_guard<std::mutex> lock(mutex);
     RoutingTableEntryTupel result = RoutingTable::getEntryAt(wantedPosition);
+    return result;
+}
+
+std::string TestbedRoutingTable::toString(int position) {
+    std::lock_guard<std::mutex> lock(mutex);
+    std::string result = RoutingTable::toString(position);
     return result;
 }
 
