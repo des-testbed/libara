a8d8d0430b75eef0da4a5f6766d0412007835bb3
Michael Frey <frey@inf.fu-berlin.de>
2014-07-07 14:59:05 +0200

    changed behavior of class TestbedPacketFactory

--- testbed/TestbedPacketFactory.cpp
+++ testbed/TestbedPacketFactory.cpp
@@ -38,111 +38,66 @@ TestbedPacket* TestbedPacketFactory::makePacket(dessert_msg_t* message) {
     // TODO: check the payloadsize (hlen, hlen + plen, ...?)
     int payloadSize = message->hlen;
 
-    return new TestbedPacket(source, destination, source, packetType, sequenceNumber, ttl, message, payloadSize);
+    TestbedPacket* packet = new TestbedPacket(source, destination, source, packetType, sequenceNumber, ttl);
+    packet->setMessage(message);
+    packet->setPayloadLength(payloadSize);
+
+    return packet;
 }
 
 TestbedPacket* TestbedPacketFactory::makeNewPacket(dessert_msg_t* message) {
     return this->makePacket(message);
 }
 
-Packet* TestbedPacketFactory::makeClone(const Packet* packet) {
-    const TestbedPacket *originalPacket = dynamic_cast<const TestbedPacket*>(packet);
-    return makePacket(originalPacket->getSource(), originalPacket->getDestination(), originalPacket->getSender(), originalPacket->getType(), originalPacket->getSequenceNumber(), originalPacket->getTTL(), originalPacket->getMessage(), originalPacket->getPayloadLength(), originalPacket->getPreviousHop());
+TestbedPacket* TestbedPacketFactory::makeClone(const TestbedPacket* packet) {
+    if (packet->getMessage() != nullptr){
+        TestbedPacket* result = this->makePacket(packet->getSource(), packet->getDestination(), packet->getSender(), packet->getType(), packet->getSequenceNumber(), packet->getTTL());
+        result->setPreviousHop(packet->getPreviousHop());
+        result->setMessage(packet->getMessage());
+        result->setPayloadLength(packet->getPayloadLength());
+        return result;
+    }
+    return makePacket(packet->getSource(), packet->getDestination(), packet->getSender(), packet->getType(), packet->getSequenceNumber(), packet->getTTL(), packet->getPayload(), packet->getPayloadLength(), packet->getPreviousHop());
 }
 
 TestbedPacket* TestbedPacketFactory::makeDataPacket(AddressPtr source, AddressPtr destination, unsigned int sequenceNumber, dessert_msg_t* payload, unsigned int payloadSize) {
-    return makePacket(source, destination, source, PacketType::DATA, sequenceNumber, this->maxHopCount, payload, payloadSize);
+    TestbedPacket* packet = this->makePacket(source, destination, source, PacketType::DATA, sequenceNumber, maxHopCount);
+    packet->setMessage(payload);
+    packet->setPayloadLength(payloadSize);
+    return packet;
 }
 
 TestbedPacket* TestbedPacketFactory::makeDataPacket(AddressPtr source, AddressPtr destination, unsigned int sequenceNumber, const char* payload, unsigned int payloadSize) {
     return makePacket(source, destination, source, PacketType::DATA, sequenceNumber, this->maxHopCount, (const char*)payload, payloadSize);
 }
-
+/*
 TestbedPacket* TestbedPacketFactory::makePacket(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, dessert_msg_t* payload, unsigned int payloadSize, AddressPtr previousHop) {
-    return new TestbedPacket(source, destination, sender, type, seqNr, ttl, payload, payloadSize);
+    TestbedPacket* packet = new TestbedPacket(source, destination, sender, type, seqNr, ttl, payload, payloadSize);
+    if(previousHop != nullptr) {
+        packet->setPreviousHop(previousHop);
+    }
+    return packet;
 }
+*/
 
 TestbedPacket* TestbedPacketFactory::makePacket(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, const char* payload, unsigned int payloadSize, AddressPtr previousHop) {
-    return new TestbedPacket(source, destination, sender, type, seqNr, ttl, payload, payloadSize);
-}
-
-dessert_msg_t* TestbedPacketFactory::makeDessertMessage(const Packet* packet, dessert_meshif_t* interface, AddressPtr nextHop) {
-    assert(packet != nullptr);
-
-    /// check the type of the packet
-    if (packet->getType() == PacketType::FANT) {
-        return this->makeFANT(packet);
-    } else if (packet->getType() == PacketType::BANT) {
-        return this->makeBANT(packet);
-    } else if (packet->getType() == PacketType::DATA){
-        /// first we have to convert the packet
-        const TestbedPacket* testbedPacket = dynamic_cast<const TestbedPacket*>(packet);
-        /// simply return the previously stored dessert message 
-        dessert_msg_t* message = testbedPacket->getMessage();
-        /// TODO: update the data, sequence number, etc.
-        
-        return message;
-    } else {
-        throw Exception("unsupported packet type");
+    TestbedPacket* packet = new TestbedPacket(source, destination, sender, type, seqNr, ttl, payload, payloadSize);
+    if(previousHop != nullptr) {
+        packet->setPreviousHop(previousHop);
     }
+    return packet;
 }
 
-dessert_msg_t* TestbedPacketFactory::makeFANT(const Packet *packet){
-    return makeAntAgent(packet);
+TestbedPacket* TestbedPacketFactory::makeFANT(TestbedAddressPtr source, TestbedAddressPtr destination, unsigned int sequenceNumber){
+    return this->makePacket(source, destination, source, PacketType::FANT, sequenceNumber, maxHopCount);
 }
 
-dessert_msg_t* TestbedPacketFactory::makeBANT(const Packet *packet){
-    return makeAntAgent(packet);
+TestbedPacket* TestbedPacketFactory::makeBANT(const Packet *packet, unsigned int sequenceNumber){
+    return this->makePacket(packet->getDestination(), packet->getSource(), packet->getDestination(), PacketType::BANT, sequenceNumber, maxHopCount);
 }
 
-dessert_msg_t* TestbedPacketFactory::makeAntAgent(const Packet *packet){
-    ara_address_t address;
-    dessert_msg_t* ant = nullptr;
-    dessert_ext_t* extension = nullptr;
-    struct ether_header* ethernetHeader = nullptr;
-
-    dessert_msg_new(&ant);
-
-    /// set sequence number, ttl and type
-    ant->u16 = htons(packet->getSequenceNumber());
-    ant->ttl = (uint8_t)packet->getTTL();
-    ant->u8 |= packet->getType();
-
-    /// add user extension
-    dessert_msg_addext(ant, &extension, DESSERT_EXT_ETH, ETHER_HDR_LEN);
-    ethernetHeader = (struct ether_header*) extension->data;
-    memcpy(ethernetHeader->ether_shost, DESSERT_LOCAL_ADDRESS, ETHER_ADDR_LEN);
-    memcpy(ethernetHeader->ether_dhost, DESSERT_BROADCAST_ADDRESS, ETHER_ADDR_LEN);
-
-    if (packet->getType() == PacketType::FANT) {
-        dessert_msg_addext(ant, &extension, ARA_EXT_FANT, ETHER_ADDR_LEN + 4);
-    } else if(packet->getType() == PacketType::BANT) {
-        dessert_msg_addext(ant, &extension, ARA_EXT_BANT, ETHER_ADDR_LEN + 4);
-    } else {
-        throw Exception("Unsupported packet type in makeAntAgent() method");
-    } 
-
-    /// get the address
-    std::shared_ptr<TestbedAddress> testbedAddress = std::dynamic_pointer_cast<TestbedAddress>(packet->getSource());
-    std::copy(testbedAddress->getDessertValue(), testbedAddress->getDessertValue() + ETHER_ADDR_LEN, address);
-    memcpy(extension->data, address, sizeof(ara_address_t));
-
-    if (packet->getType() == PacketType::FANT) {
-        memcpy(extension->data + ETHER_ADDR_LEN, "FANT", 4);
-    } else if(packet->getType() == PacketType::BANT) {
-        memcpy(extension->data + ETHER_ADDR_LEN, "BANT", 4);
-//      ara_maintainroute_stamp(addr);
-    } else {
-        throw Exception("Unsupported packet type in makeAntAgent() method");
-    }
-    /// TODO: Check
-    /* for loop vs duplicate detection */
-//  dessert_msg_trace_initiate(bant, DESSERT_EXT_TRACE_REQ, DESSERT_MSG_TRACE_HOST);
-
-    /// TODO: DES-ARA sets ara_ant_size to 128, should better check this
-    dessert_msg_dummy_payload(ant, 128);
-
-    return ant;
+TestbedPacket* TestbedPacketFactory::makeAcknowledgmentPacket(const Packet* originalPacket, TestbedAddressPtr sender){
+    return makePacket(originalPacket->getSource(), originalPacket->getDestination(), sender, PacketType::ACK, originalPacket->getSequenceNumber(), maxHopCount);
 }
 
 TESTBED_NAMESPACE_END
--- tests/testbed/TestbedARAClientTest.cpp
+++ tests/testbed/TestbedARAClientTest.cpp
@@ -11,29 +11,53 @@
 #include "BestPheromoneForwardingPolicy.h"
 #include "Environment.h"
 #include "TestbedARAClient.h"
-
+#include "TestbedPacketFactory.h"
 
 TESTBED_NAMESPACE_BEGIN
 
 TEST_GROUP(TestbedARAClientTest) {
     TestbedARAClient* client;
+    TestbedPacketFactory* packetFactory;
 
     void setup() {
         RoutingTableMock* routingTable = new RoutingTableMock();
-        BasicConfiguration config = BasicConfiguration(routingTable, new PacketFactory(15), new ExponentialEvaporationPolicyMock(),
-                                        new LinearPathReinforcementPolicy(5.0), new BestPheromoneForwardingPolicy(routingTable), 5.0);
+        packetFactory = new TestbedPacketFactory(15);
+        std::shared_ptr<PacketTrap> packetTrap = std::make_shared<PacketTrap>(routingTable);
+        BasicConfiguration config = BasicConfiguration(routingTable, packetFactory, new ExponentialEvaporationPolicyMock(),
+                                        new LinearPathReinforcementPolicy(5.0), new BestPheromoneForwardingPolicy(routingTable), packetTrap, 5.0);
         client = new TestbedARAClient(config);
     }
 
     void teardown() {
         /// TODO: check if that's a bright idea!
         Environment::setClock(nullptr);
+        /**
+         * we don't delete the packetFactory by ourselves, since the client
+         * takes care of that
+         */
         delete client;
     }
 };
 
 TEST(TestbedARAClientTest, configTest) {
     CHECK(client->getPacketFactory() != nullptr);
+}
+/*
+TEST(TestbedARAClientTest, packetGetsTrappedIfNotDeliverable) {
+    NetworkInterfaceMock* interface = client->createNewNetworkInterfaceMock();
+    PacketMock* packet = new PacketMock();
+    packet->setSource(interface->getLocalAddress());
+
+    CHECK(routingTable->isDeliverable(packet) == false);
+    client->sendPacket(packet);
+    CHECK(packetTrap->contains(packet));
+}
+*/
+
+TEST(TestbedARAClientTest, getNextSequenceNumber) {
+    for (unsigned int i = 1; i < 100; i++) {
+        CHECK_EQUAL(i, client->getNextSequenceNumber());
+    }
 }
 
 TESTBED_NAMESPACE_END
