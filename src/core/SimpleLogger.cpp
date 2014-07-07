4c704dde26a84f26e9075b9ec136b3f5e232214d
Michael Frey <frey@inf.fu-berlin.de>
2014-07-07 15:11:18 +0200

    made changes to cout/cerr in class SimpleLogger

--- src/core/SimpleLogger.cpp
+++ src/core/SimpleLogger.cpp
@@ -16,7 +16,8 @@ namespace ARA {
 void SimpleLogger::performLoggingAction(const std::string &logMessage, Level level, va_list args) const {
     char buffer[512]; // FIXME a dynamic buffer size would be awesome
     vsprintf(buffer, logMessage.c_str(), args);
-    cout << instanceName << "  [" << getLevelString(level) << "] " << buffer << "\n";
+    //cout << instanceName << "  [" << getLevelString(level) << "] " << buffer << "\n";
+    cerr << instanceName << "  [" << getLevelString(level) << "] " << buffer << "\n";
 }
 
 } /* namespace ARA */
