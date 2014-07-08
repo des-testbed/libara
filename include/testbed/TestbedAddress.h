a2d3ad2539815b31d40f202ac29b9b9d5831e66f
Michael Frey <frey@inf.fu-berlin.de>
2014-07-08 16:34:57 +0200

    added a mutex for access to members of class TestbedAddress

--- include/testbed/TestbedAddress.h
+++ include/testbed/TestbedAddress.h
@@ -8,6 +8,7 @@
 #include "Address.h"
 #include "Testbed.h"
 
+#include <mutex>
 #include <sstream>
 #include <iterator>
 #include <algorithm>
@@ -16,12 +17,36 @@ TESTBED_NAMESPACE_BEGIN
 
 class TestbedAddress : public Address {
     public:
+        /**
+         * The constructor of the class takes an unsigned char array as an
+         * argument and stores it as a ara_address_t (which is typedef to
+         * an unsigned char aray of size size 6)
+         *
+         * @param address The address which should be transformed into a
+         * TestbedAddress
+         */
         TestbedAddress(u_char* address);
+
         ~TestbedAddress(){ }
 
+        /**
+         * @see Address::toString() const
+         */
         virtual std::string toString() const;
+
+        /**
+         * @see Address::equals(const Address* otherAddress) const
+         */
         virtual bool equals(const Address* otherAddress) const;
+
+        /**
+         * @see Address::equals(const std::shared_ptr<Address> otherAddress) const
+         */
         virtual bool equals(const std::shared_ptr<Address> otherAddress) const; 
+
+        /**
+         * @see Address::getHashValue() const
+         */
         virtual size_t getHashValue() const;
 
         /**
@@ -30,7 +55,15 @@ class TestbedAddress : public Address {
         u_int8_t* getDessertValue() const;
 
     private:
+        /**
+         *
+         */
         ara_address_t address;
+
+        /**
+         * A mutex to protect the members of class TestbedAddress
+         */
+        mutable std::mutex addressMutex;
 };
 
 TESTBED_NAMESPACE_END
--- testbed/TestbedAddress.cpp
+++ testbed/TestbedAddress.cpp
@@ -11,6 +11,7 @@ TestbedAddress::TestbedAddress(u_char* address){
 }
 
 std::string TestbedAddress::toString() const {
+    std::lock_guard<std::mutex> lock(addressMutex);
     std::ostringstream result;
     /// set output to hex values
     result << std::hex;
@@ -23,6 +24,7 @@ std::string TestbedAddress::toString() const {
 }
 
 bool TestbedAddress::equals(const Address* otherAddress) const{
+    //std::lock_guard<std::mutex> lock(addressMutex);
     const TestbedAddress* otherTestbedAddress = dynamic_cast<const TestbedAddress*>(otherAddress);
 
     if (otherTestbedAddress == nullptr) {
@@ -34,16 +36,20 @@ bool TestbedAddress::equals(const Address* otherAddress) const{
 }
 
 bool TestbedAddress::equals(const std::shared_ptr<Address> otherAddress) const {
+    //std::lock_guard<std::mutex> lock(addressMutex);
     return this->equals(otherAddress.get());
 }
 
 size_t TestbedAddress::getHashValue() const {
+    //std::lock_guard<std::mutex> lock(addressMutex);
     int leastSignificantByte = address[5];
     int nextLeastSignificantByte = address[4];
     return leastSignificantByte * 256 + nextLeastSignificantByte;
 }
 
 u_int8_t* TestbedAddress::getDessertValue() const {
+    // FIXME: we pass the pointer to something else ... (not good!)
+    //std::lock_guard<std::mutex> lock(addressMutex);
     //TODO: Find a better way to return acceptable value for cross platform
     return (u_int8_t*) this->address;
 }
