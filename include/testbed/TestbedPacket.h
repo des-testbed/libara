d8a92cca9bb9f59e53320b4862cc811fb45ca7d0
Michael Frey <frey@inf.fu-berlin.de>
2014-07-30 17:48:49 +0200

    added additional mutexes in class TestbedPacket

--- include/testbed/TestbedPacket.h
+++ include/testbed/TestbedPacket.h
@@ -23,7 +23,7 @@ TESTBED_NAMESPACE_BEGIN
 class TestbedPacket : public Packet {
     public:
         /**
-         * @see Packet::TestbedPacket(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, const char* payload=nullptr, unsigned int payloadSize=0);
+         * @see Packet::Packet(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, const char* payload=nullptr, unsigned int payloadSize=0);
          */
         TestbedPacket(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, const char* payload=nullptr, unsigned int payloadSize=0);
 
@@ -66,8 +66,17 @@ class TestbedPacket : public Packet {
 
     private:
         mutable std::mutex mutex;
-        /// we simply store the message received over the wire/air
+
+	    mutable std::mutex senderMutex;
+	    mutable std::mutex sourceMutex;
+	    mutable std::mutex destinationMutex;
+
         dessert_msg_t* message = nullptr;
+
+        /**
+         * The payload of the packet.
+         */
+        void* payload = nullptr;
 };
 
 TESTBED_NAMESPACE_END
--- testbed/TestbedPacket.cpp
+++ testbed/TestbedPacket.cpp
@@ -3,23 +3,24 @@
  */
 
 #include "TestbedPacket.h"
+#include "TestbedPacketFactory.h"
 
 TESTBED_NAMESPACE_BEGIN
 
 TestbedPacket::TestbedPacket(AddressPtr source, AddressPtr destination, AddressPtr sender, char type, unsigned int seqNr, int ttl, const char* payload, unsigned int payloadSize) : Packet(source, destination, sender, type, seqNr, ttl, payload, payloadSize) { }
 
 AddressPtr TestbedPacket::getSource() const {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(sourceMutex);
     return Packet::getSource();
 }
 
 AddressPtr TestbedPacket::getDestination() const {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(destinationMutex);
     return Packet::getDestination();
 }
 
 AddressPtr TestbedPacket::getSender() const {
-    std::lock_guard<std::mutex> lock(mutex);
+    std::lock_guard<std::mutex> lock(senderMutex);
     return Packet::getSender();
 }
 
@@ -44,9 +45,19 @@ void TestbedPacket::setMessage(dessert_msg_t* newMessage) {
 
     if (message != nullptr){
        dessert_msg_destroy(message);
+       payloadSize = 0;
     }
 
     message = newMessage;
+
+    /// check if the packet has actually payload  
+    if (newMessage->plen > 0) {
+        /// get the payload and update the size of the payload
+        if ((payloadSize = dessert_msg_getpayload(newMessage, &payload)) == 0) {
+            // DEBUG:
+            std::cerr << "[TestbedPacket::setMessage] saving payload failed" << std::endl;
+        }
+    }
 }
 
 void TestbedPacket::setPayloadLength(unsigned int newPayloadLength){
@@ -57,6 +68,8 @@ void TestbedPacket::setPayloadLength(unsigned int newPayloadLength){
 
 
 dessert_msg_t* TestbedPacket::toDessertMessage() const {
+    std::lock_guard<std::mutex> lock(mutex);
+
     ara_address_t address;
     dessert_msg_t* packet = nullptr;
     dessert_ext_t* extension = nullptr;
@@ -76,19 +89,24 @@ dessert_msg_t* TestbedPacket::toDessertMessage() const {
         /// set time to live 
         packet->ttl = (uint8_t)ttl;
         /// set the type
-        packet->u8 |= type;
-        // int antSize = ;
-        
+        //packet->u8 |= type;
+        packet->u8 = type;
+        /// TODO: ant size in DES-ARA set to 128 by means of des-ara.conf
+        int antSize = 128;
+
         dessert_msg_addext(packet, &extension, DESSERT_EXT_ETH, ETHER_HDR_LEN);
         ethernetHeader = (struct ether_header*) extension->data;
+        /// set the previous hop
+        memcpy(ethernetHeader->ether_shost, DESSERT_LOCAL_ADDRESS, ETHER_ADDR_LEN);
+
+        /*
+        u_int8_t* src = sourceAddress->getDessertValue();
+        u_int8_t* dest = destinationAddress->getDessertValue();
+        */
 
         if ((type == PacketType::FANT) || (type == PacketType::BANT)) {
-            u_int8_t* src = sourceAddress->getDessertValue();
-            u_int8_t* dest = destinationAddress->getDessertValue();
-
-            /// TODO: better check this
-            memcpy(ethernetHeader->ether_shost, src, ETHER_ADDR_LEN);
-            memcpy(ethernetHeader->ether_dhost, dest, ETHER_ADDR_LEN);
+            /// set the destination 
+            memcpy(ethernetHeader->ether_dhost, DESSERT_BROADCAST_ADDRESS, ETHER_ADDR_LEN);
 
             if (type == PacketType::FANT) {
                 dessert_msg_addext(packet, &extension, ARA_EXT_FANT, ETHER_ADDR_LEN + 4);
@@ -99,14 +117,37 @@ dessert_msg_t* TestbedPacket::toDessertMessage() const {
                 memcpy(extension->data, address, sizeof(ara_address_t));
                 memcpy(extension->data + ETHER_ADDR_LEN, "BANT", 4);
             }
-
-            // dessert_msg_dummy_payload(packet, antSize);
-
+        /*    
+        dessert_ext_t* routingExtension = nullptr;
+        dessert_msg_addext(packet, &routingExtension, DESSERT_EXT_USER, ETHER_HDR_LEN);
+        RoutingExtension* araRoutingExtension = (RoutingExtension*)routingExtension->data;
+        /// TODO: and again better check this
+        memcpy(araRoutingExtension->ara_shost, src, ETHER_ADDR_LEN);
+        memcpy(araRoutingExtension->ara_dhost, dest, ETHER_ADDR_LEN);
+*/
+            dessert_msg_dummy_payload(packet, antSize);
         } else if (type == PacketType::DATA){
-            if (message != nullptr) {
-                /// we update the sequence number
-                message->u16 = htons(seqNr);
-                return message;
+            void* tempPayload = nullptr;
+
+            /**
+             * TODO: this might cause a lot of trouble, depending what is doing
+             * the free or (delete). We assume that libdessert takes care of the
+             * packet and the payload and hence, it is very likely that free is
+             * called.
+             */
+            if ((tempPayload = malloc(payloadSize)) != nullptr){
+                /// copy over the original payload
+                memcpy(tempPayload, payload, payloadSize);
+
+                if (dessert_msg_addpayload(packet, &tempPayload, payloadSize) == DESSERT_OK) {
+
+                } else {
+                   // DEBUG:
+                   std::cerr << "[TestbedPacket::toDessertMessage] adding payload failed" << std::endl;
+                }
+            } else {
+                // DEBUG:
+                std::cerr << "[TestbedPacket::toDessertMessage] memory allocation for payload failed" << std::endl;
             }
         } else if (type == PacketType::PANT){
 
@@ -125,11 +166,12 @@ dessert_msg_t* TestbedPacket::toDessertMessage() const {
         }
     /// creating a dessert_msg_t failed
     } else {
-
-
+        // DEBUG:
+        std::cerr << "[TestbedPacket::toDessertMessage] creating a new dessert message failed" << std::endl;
     }
 
     return packet;
 }
+
 
 TESTBED_NAMESPACE_END
