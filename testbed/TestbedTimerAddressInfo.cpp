7c6b7b87451bb72f2cd8c976aee7d24b781d8ab5
Michael Frey <frey@inf.fu-berlin.de>
2014-07-30 17:54:00 +0200

    disabled getAddress method in TestbedTimerAddressInfo

--- testbed/TestbedTimerAddressInfo.cpp
+++ testbed/TestbedTimerAddressInfo.cpp
@@ -8,10 +8,12 @@ TESTBED_NAMESPACE_BEGIN
 
 TestbedTimerAddressInfo::TestbedTimerAddressInfo(std::shared_ptr<TestbedAddress> destination) : TimerAddressInfo(destination) { }
 
-std::shared_ptr<TestbedAddress> TestbedTimerAddressInfo::getAddress() {
-    std::lock_guard<std::mutex> lock(addressMutex);
-    std::shared_ptr<TestbedAddress> result = std::dynamic_pointer_cast<TestbedAddress>(destination);
-    return result;
+//std::shared_ptr<TestbedAddress> TestbedTimerAddressInfo::getAddress() {
+std::shared_ptr<Address> TestbedTimerAddressInfo::getAddress() {
+//    std::lock_guard<std::mutex> lock(addressMutex);
+//    std::shared_ptr<TestbedAddress> result = std::dynamic_pointer_cast<TestbedAddress>(destination);
+  //  return result;
+      return destination;
 }
 
 TESTBED_NAMESPACE_END
