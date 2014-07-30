7141976078fbbc60983f30af818230f5786a4050
Michael Frey <frey@inf.fu-berlin.de>
2014-07-30 18:02:07 +0200

    added fix for issue in class BestPheromoneForwardingPolicy

--- src/core/forwarding/BestPheromoneForwardingPolicy.cpp
+++ src/core/forwarding/BestPheromoneForwardingPolicy.cpp
@@ -15,20 +15,29 @@ BestPheromoneForwardingPolicy::BestPheromoneForwardingPolicy(RoutingTable* routi
 
 NextHop* BestPheromoneForwardingPolicy::getNextHop(const Packet* packet) {
     RoutingTableEntryList possibleNextHops = routingTable->getPossibleNextHops(packet);
-    if(possibleNextHops.empty()) {
-        throw Exception("Could not determine next hop: there are no known routes to the destination");
-    }
-
     RoutingTableEntry* bestEntry = nullptr;
-    float globalMaximum = 0;
-    for(auto& possibleNextHop: possibleNextHops) {
-        if(possibleNextHop->getPheromoneValue() > globalMaximum) {
-            bestEntry = possibleNextHop;
-            globalMaximum = bestEntry->getPheromoneValue();
+
+    if (possibleNextHops.empty()) {
+        throw Exception("Could not determine next hop: there are no known routes to the destination");
+    } else {
+        float globalMaximum = 0;
+
+        for (auto& possibleNextHop: possibleNextHops) {
+            if (possibleNextHop->getPheromoneValue() > globalMaximum) {
+                bestEntry = possibleNextHop;
+                globalMaximum = bestEntry->getPheromoneValue();
+            }
         }
+
+        // this should never happen, but nobody should say never
+        if ((globalMaximum == 0) || bestEntry == nullptr) {
+            return nullptr;
+        }
+
+        return bestEntry->getNextHop();
     }
 
-    return bestEntry->getNextHop();
+    return nullptr;
 }
 
 ARA_NAMESPACE_END
