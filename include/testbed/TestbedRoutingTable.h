fbbbdc928344a8349ade77c55d91d0c1824eaabf
Michael Frey <frey@inf.fu-berlin.de>
2014-07-30 18:05:07 +0200

    (not yet finished) changes to class TestbedRoutingTable

--- include/testbed/TestbedRoutingTable.h
+++ include/testbed/TestbedRoutingTable.h
@@ -5,15 +5,15 @@
 #define TESTBED_ROUTINGTABLE_H_
 
 #include "ARAMacros.h"
+#include "Clock.h"
 #include "Packet.h"
 #include "Address.h"
 #include "Testbed.h" 
+#include "Environment.h"
+#include "RoutingTable.h"
 #include "NetworkInterface.h"
 #include "EvaporationPolicy.h"
-#include "RoutingTable.h"
 #include "ExponentialEvaporationPolicy.h"
-#include "RoutingTable.h"
-#include "Clock.h"
 
 #include <mutex>
 
@@ -29,27 +29,91 @@ class TestbedRoutingTable : public RoutingTable {
         TestbedRoutingTable();
         virtual ~TestbedRoutingTable();
 
+        /**
+         * @see RoutingTable::getPheromoneValue(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface)
+         */
         float getPheromoneValue(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface);
+
+        /**
+         * @see RoutingTable::update(AddressPtr destination, RoutingTableEntry* entry);
+         */
         void update(AddressPtr destination, RoutingTableEntry* entry);
+
+        /**
+         * @see RoutingTable::removeEntry(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface)
+         */
         void removeEntry(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface);
         
+        /**
+         * @see RoutingTable::isDeliverable(AddressPtr destination)
+         */
         bool isDeliverable(AddressPtr destination);
+
+        /**
+         * @see RoutingTable::isDeliverable(const Packet* packet)
+         */
         bool isDeliverable(const Packet* packet);
 
+        /**
+         * @see RoutingTable::exists(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface)
+         */
         bool exists(AddressPtr destination, AddressPtr nextHop, NetworkInterface* interface);
+
+        /**
+         * @see RoutingTable::getPossibleNextHops(const Packet* packet)
+         */
         RoutingTableEntryList getPossibleNextHops(const Packet* packet);
+
+        /**
+         * @see RoutingTable::getPossibleNextHops(AddressPtr destination)
+         */
         RoutingTableEntryList getPossibleNextHops(AddressPtr destination);
 
-//        void triggerEvaporation();
+        /**
+         * @see RoutingTable::triggerEvaporation()
+         */
+        void triggerEvaporation();
+
+        /**
+         * @see RoutingTable::getTotalNumberOfEntries() const
+         */
         unsigned int getTotalNumberOfEntries() const;
+
+        /**
+         * @see RoutingTable::getEntryAt(int position) const
+         */
         RoutingTableEntryTupel getEntryAt(int position) const;
+
+        /**
+         * @see RoutingTable::getAllRoutesThatLeadOver(AddressPtr nextHop) const;
+         */
         std::deque<RoutingTableEntryTupel> getAllRoutesThatLeadOver(AddressPtr nextHop) const;
     
+        /**
+         * @see RoutingTable::toString(int position=-1);
+         */
         std::string toString(int position=-1);
 
     private:
+
+        /**
+         * This derived method does the same as the method in the base class, but uses
+         * locks and mutexes in order to protect critical sections. The method is
+         * called by the TestbedRoutingTable::triggerEvaporation() method. Unfortunately 
+         * adding a mutex and calling RoutingTable::triggerEvaporation() did not work 
+         * out (and so does a recursive mutex). However, we should come up with a
+         * better solution which is not the the same code + plus mutex/lock
+         *
+         * FIXME: This method required a fix in terms of design (see comment above)
+         */
+        void applyEvaporation(Time* currentTime);
+
+        /**
+         * The mutex protects the routing table of class RoutingTable
+         */
         mutable std::mutex mutex;
 };
 
 TESTBED_NAMESPACE_END
+
 #endif /* TESTBED_ROUTINGTABLE_H_ */
--- testbed/TestbedRoutingTable.cpp
+++ testbed/TestbedRoutingTable.cpp
@@ -54,12 +54,56 @@ bool TestbedRoutingTable::exists(AddressPtr destination, AddressPtr nextHop, Net
     bool result = RoutingTable::exists(destination, nextHop, interface);
     return result;
 }
-/*
+
 void TestbedRoutingTable::triggerEvaporation() {
-    std::lock_guard<std::mutex> lock(mutex);
-    RoutingTable::triggerEvaporation();
+//    std::lock_guard<std::mutex> lock(mutex);
+    Time* currentTime = Environment::getClock()->makeTime();
+    currentTime->setToCurrentTime();
+
+    if (lastAccessTime == nullptr) {
+        lastAccessTime = currentTime;
+    } else {
+        applyEvaporation(currentTime);
+    }
 }
-*/
+
+void TestbedRoutingTable::applyEvaporation(Time* currentTime){
+    long timeDifference = currentTime->getDifferenceInMilliSeconds(lastAccessTime);
+    delete currentTime;
+
+    if (evaporationPolicy->isEvaporationNecessary(timeDifference)) {
+        lastAccessTime->setToCurrentTime();
+
+        std::lock_guard<std::mutex> lock(mutex);
+        RoutingTableMap::iterator i = table.begin();
+        while (i!=table.end()) {
+            std::pair<AddressPtr const, std::shared_ptr<RoutingTableEntryList> > entryPair = *i;
+            AddressPtr destination = entryPair.first;
+	        std::shared_ptr<RoutingTableEntryList> nextHopsForDestination = entryPair.second;
+
+            // apply evaporation to all next hops for that destination
+            RoutingTableEntryList::iterator j = nextHopsForDestination->begin();
+            while (j != nextHopsForDestination->end()) {
+                RoutingTableEntry* entry = *j;
+                float newPheromoneValue = evaporationPolicy->evaporate(entry->getPheromoneValue(), timeDifference);
+                if (newPheromoneValue > 0) {
+                    entry->setPheromoneValue(newPheromoneValue);
+                    j++;
+                } else {
+                    delete entry;
+                    j = nextHopsForDestination->erase(j); // this does not invalidate the iterator, because j is set to the valid return value of erase (will point to end() if empty)
+                }
+            }
+
+            if (nextHopsForDestination->empty()) {
+                i = table.erase(i); // this does not invalidate the iterator, because i is set to the valid return value of erase (will point to end() if empty)
+            } else {
+                i++;
+            }
+        }
+    }
+}
+
 unsigned int TestbedRoutingTable::getTotalNumberOfEntries() const {
     std::lock_guard<std::mutex> lock(mutex);
     unsigned int result = RoutingTable::getTotalNumberOfEntries();
