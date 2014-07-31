719c2637bf3c076a70ce9ebedb944b1c7096bfb6
Michael Frey <mail@mfrey.net>
2014-07-31 14:04:36 +0200

    added check on size of nrOfPossibleNextHops

--- src/core/forwarding/StochasticForwardingPolicy.cpp
+++ src/core/forwarding/StochasticForwardingPolicy.cpp
@@ -13,40 +13,52 @@ StochasticForwardingPolicy::StochasticForwardingPolicy(RoutingTable* routingTabl
 
 NextHop* StochasticForwardingPolicy::getNextHop(const Packet* packet) {
     std::deque<RoutingTableEntry*> possibleNextHops = routingTable->getPossibleNextHops(packet);
-    if(possibleNextHops.empty()) {
+
+    if (possibleNextHops.empty()) {
         throw Exception("Could not determine next hop: there are no known routes to the destination");
     }
 
-    unsigned int nrOfPossibleNextHops = possibleNextHops.size();
-    float cumulativeSum[nrOfPossibleNextHops] = { };
-    float probabilities[nrOfPossibleNextHops] = { };
-    float pheromoneValues[nrOfPossibleNextHops] = { };
+    unsigned int nrOfPossibleNextHops = 0;
+    // clang complains about an (maybe) unitialized variable nrOfPossibleNextHops
+    nrOfPossibleNextHops = possibleNextHops.size();
 
-    float sumOfPheromoneValues = 0;
-    for(unsigned int i = 0; i < nrOfPossibleNextHops; i++){
-        pheromoneValues[i] = possibleNextHops.at(i)->getPheromoneValue();
-        sumOfPheromoneValues += pheromoneValues[i];
+    if (nrOfPossibleNextHops > 0) {
+
+        float cumulativeSum[nrOfPossibleNextHops] = { };
+        float probabilities[nrOfPossibleNextHops] = { };
+        float pheromoneValues[nrOfPossibleNextHops] = { };
+
+        float sumOfPheromoneValues = 0;
+
+        for (unsigned int i = 0; i < nrOfPossibleNextHops; i++) {
+            pheromoneValues[i] = possibleNextHops.at(i)->getPheromoneValue();
+            sumOfPheromoneValues += pheromoneValues[i];
+        }
+
+        // calculate the probabilities
+        for (unsigned int i = 0; i < nrOfPossibleNextHops; i++) {
+            probabilities[i] = pheromoneValues[i] / sumOfPheromoneValues;
+        }
+
+        // compute the cumulative sum
+        std::partial_sum(probabilities, probabilities + nrOfPossibleNextHops, cumulativeSum);
+
+        // get a random number between 0.0 and 1.0
+        float randomNumber = this->getRandomNumber();
+
+        int nodeIndex = 0;
+
+        while (randomNumber > cumulativeSum[nodeIndex]) {
+            nodeIndex += 1;
+        }
+
+        NextHop* result = possibleNextHops.at(nodeIndex)->getNextHop();
+        return result;
     }
+    // DEBUG:
+    std::cerr << "[StochasticForwardingPolicy::getNextHop] no next hop available" << std::endl;
 
-    // calculate the probabilities
-    for(unsigned int i = 0; i < nrOfPossibleNextHops; i++){
-        probabilities[i] = pheromoneValues[i] / sumOfPheromoneValues;
-    }
-
-    // compute the cumulative sum
-    std::partial_sum(probabilities, probabilities + nrOfPossibleNextHops, cumulativeSum);
-
-
-    // get a random number between 0.0 and 1.0
-    float randomNumber = this->getRandomNumber();
-
-    int nodeIndex = 0;
-    while(randomNumber > cumulativeSum[nodeIndex]){
-        nodeIndex += 1;
-    }
-
-    NextHop* result = possibleNextHops.at(nodeIndex)->getNextHop();
-    return result;
+    return nullptr;
 }
 
 void StochasticForwardingPolicy::initializeRandomNumberGenerator(unsigned int seed) {
