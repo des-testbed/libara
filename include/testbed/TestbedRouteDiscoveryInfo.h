3c546c5ebb4d373e0e830a41999dc0ca5e0ed91c
Michael Frey <frey@inf.fu-berlin.de>
2014-08-26 13:50:15 +0200

    added missing files on TestbedRouteDiscoveryInfo and TimerAddressInfo

---
+++ include/testbed/TestbedRouteDiscoveryInfo.h
@@ -0,0 +1,33 @@
+/*
+ * $FU-Copyright$
+ */
+
+#ifndef TESTBED_ROUTE_DISCOVERY_INFO_H_
+#define TESTBED_ROUTE_DISCOVERY_INFO_H_
+
+#include "Testbed.h"
+#include "TestbedPacket.h"
+#include "RouteDiscoveryInfo.h"
+
+#include <mutex>
+
+TESTBED_NAMESPACE_BEGIN
+
+/**
+ * A TestbedRouteDiscoveryInfo object holds data about a running route discovery.
+ */
+class TestbedRouteDiscoveryInfo : public RouteDiscoveryInfo {
+    public:
+        TestbedRouteDiscoveryInfo(const TestbedPacket* associatedPacket);
+
+        const TestbedPacket* getPacket();
+        int getNumberOfRetries();
+        void setNumberOfRetries(int newNumberOfRetries);
+
+    private:
+        std::recursive_mutex packetMutex;
+};
+
+TESTBED_NAMESPACE_END
+
+#endif // TESTBED_ROUTE_DISCOVERY_INFO_H_
---
+++ src/time/TimerAddressInfo.cpp
@@ -0,0 +1,18 @@
+/*
+ * $FU-Copyright$
+ */
+
+#include "TimerAddressInfo.h"
+
+ARA_NAMESPACE_BEGIN
+
+TimerAddressInfo::TimerAddressInfo(AddressPtr destination) {
+    this->destination = destination;
+    this->setType(std::string("TimerAddressInfo"));
+}
+
+std::shared_ptr<Address> TimerAddressInfo::getDestination() {
+    return this->destination;
+}
+
+ARA_NAMESPACE_END
