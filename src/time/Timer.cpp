81f64f0c80ecb06501aa0fcc676da7e0924d3d4b
Michael Frey <frey@inf.fu-berlin.de>
2014-09-17 16:02:48 +0200

    added try/catch block in class Timer in notifyAllListeners method

--- src/time/Timer.cpp
+++ src/time/Timer.cpp
@@ -5,6 +5,8 @@
 #include "Timer.h"
 
 #include <thread>
+#include <system_error>
+    
 
 using namespace ARA;
 
@@ -31,12 +33,19 @@ void Timer::addTimeoutListener(TimeoutEventListener* listener) {
 }
 
 void Timer::notifyAllListeners() {
-    //std::thread::id this_id = std::this_thread::get_id();
-    
     std::cerr << "[Timer::notifyAllListeners] for " <<  TimerType::getAsString(type) << std::endl;
 
     for(auto& listener: listeners) {
 	    std::weak_ptr<Timer> timer = this->getTimer();
-        listener->timerHasExpired(timer);
+
+        try {
+            listener->timerHasExpired(timer);
+        } catch (const std::system_error& error) {
+	        std::cerr << "[Timer] caught system_error " << std::endl;
+            std::cerr << "Error:    " << error.what() << std::endl;
+            std::cerr << "Code:     " << error.code().value() << std::endl;
+            std::cerr << "Category: " << error.code().category().name() << std::endl;
+            std::cerr << "Message:  " << error.code().message() << std::endl;
+        }
     }
 }
