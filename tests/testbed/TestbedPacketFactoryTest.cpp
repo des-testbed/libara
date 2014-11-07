50fcb0c083e7f651afc78760a7920e6352c148a2
Michael Frey <frey@inf.fu-berlin.de>
2014-11-07 20:32:44 +0100

    added another unit test for TestbedPacketFactory

--- tests/testbed/TestbedPacketFactoryTest.cpp
+++ tests/testbed/TestbedPacketFactoryTest.cpp
@@ -49,6 +49,31 @@ TEST(TestbedPacketFactoryTest, makeFANT) {
 }
 
 
+TEST(TestbedPacketFactoryTest, makeBANT) {
+    u_char sourceMac[ETHER_ADDR_LEN] = {0x00, 0x1f, 0x1f, 0x09, 0x09, 0xe2};
+    TestbedAddressPtr originalSource = std::make_shared<TestbedAddress>(sourceMac);
 
+    u_char destinationMac[ETHER_ADDR_LEN] = {0xfe, 0x0d, 0xb9, 0x14, 0xd8, 0xc8};
+    TestbedAddressPtr originalDestination = std::make_shared<TestbedAddress>(destinationMac);
+
+    int seqNr = 3;
+    TestbedPacket* fant = factory->makeFANT(originalSource, originalDestination, seqNr);
+
+    unsigned int newSequenceNumber = 12345;
+    TestbedPacket* bant = factory->makeBANT(fant, newSequenceNumber);
+
+    CHECK(bant->getSource()->equals(originalDestination));
+    CHECK(bant->getDestination()->equals(originalSource));
+    CHECK(bant->getSender()->equals(originalDestination));
+    CHECK(bant->getPreviousHop()->equals(originalDestination));
+    CHECK(bant->getType() == PacketType::BANT);
+
+    LONGS_EQUAL(newSequenceNumber, bant->getSequenceNumber());
+    LONGS_EQUAL(maximumHopCount, bant->getTTL());
+    LONGS_EQUAL(0, bant->getPayloadLength());
+
+    delete fant;
+    delete bant;
+}
 
 TESTBED_NAMESPACE_END
