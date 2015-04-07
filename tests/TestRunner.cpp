5932aa0b0c920b17b04df0eb1953769d739903c8
Michael Frey <frey@inf.fu-berlin.de>
2015-04-07 10:02:59 +0200

    removed Mocks for Logger and added initialization of spdlog logger in TestRunner

--- tests/TestRunner.cpp
+++ tests/TestRunner.cpp
@@ -10,16 +10,29 @@
 #include "testAPI/plugins/EnvironmentPlugin.h"
 #include <iostream>
 
+#include "spdlog/spdlog.h"
+
 using namespace ARA;
 
 int main(int ac, char** av) {
+    namespace spd = spdlog;
+
     try {
         TestRegistry* r = TestRegistry::getCurrentRegistry();
+        /// log filename is unit_test.txt
+        std::string logFile("unit_test");
+
+        auto file_logger = spd::rotating_logger_mt("file_logger", logFile, 1048576 * 5, 3, true);
+        file_logger->set_level(spd::level::trace);
+        spd::set_pattern("*** [%H:%M:%S:%e] [thread %t] %v ***");
+
         EnvironmentPlugin environmentPlugin("EnvironmentPlugin");
         r->installPlugin(&environmentPlugin);
+
         return CommandLineTestRunner::RunAllTests(ac, av);
-//    } catch(const std::bad_weak_ptr& error) {
-///         std::cout << error.what() << '\n';
+    } catch (const spd::spdlog_ex& logException) {
+        std::cerr << "creating log failed: " << logException.what() << std::endl;
+        return 1;
     } catch(ARA::Exception& exception) {
         std::cout << "\nERROR: Tests failed with ARA::Exception: " << exception.getMessage() << "\n\n";
         return 1;
